c5458ca28563e56099b1022fe80ffcf650e943ab
sup <legrand@coanalytics.net>
2016-03-29 20:31:54 -0400

    removed Graphpaper2

--- server.R
+++ server.R
@@ -347,29 +347,6 @@ output$svghtml <- renderUI({
   src<-usingDraggable(src)
   
   
-#   graphPaper2 %<c-% function(wh=c(600,600), dxy=c(50, 50), labels=TRUE ){
-#     if(showGrid==FALSE){
-#       return(NULL)
-#     }
-#     seq(0,wh[1],dxy[1])->xs
-#     seq(0,wh[2],dxy[2])->ys
-#     grph<-c(
-#       lapply(xs, function(x)line(xy1=c(x,0),xy2=c(x,wh[2]))),
-#       lapply(ys, function(y)line(xy1=c(0,y),xy2=c(wh[1],y)))
-#     )
-#     if(labels){
-#       grph<-c(grph, 
-#               lapply(xs, function(x)text(xy=c(x+2,10),x)),
-#               lapply(ys, function(y)text(xy=c(2,y),y))
-#       )
-#     }
-#     g( stroke.width=1,
-#        font.size=10,
-#        stroke="grey",
-#        grph
-#     )       
-#   }
-#     
   showPts %<c-% function(ptName){
     ptDefs<-getPtDefs()
     if(is.null(ptName)){
