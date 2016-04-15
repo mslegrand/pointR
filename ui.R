4c7b47950dc7920532ecf31b8bb1eb60270bfe10
sup <legrand@coanalytics.net>
2016-04-14 20:39:21 -0400

    cleaned up, extracted some of the styling

--- ui.R
+++ ui.R
@@ -15,12 +15,10 @@ source("coreConfig.R")
 shinyUI(fluidPage(
   singleton(tags$head(
     tags$script(src = "message-handler.js"),
-    #tags$link(rel = "stylesheet", type = "text/css", href = "bootstrap.css")
     tags$style(HTML(styleSpec))
   )),
   headerPanel(
-    h1("pointR  -An svgR programming tool- (v 0.3)", 
-       style = "font-family: 'ChunkFiveRegular'; font-style: italic; font-weight: 500; line-height: 1.1;  color: #4d3a7d;")
+    h1("pointR  -An svgR programming tool- (v 0.3)", style = cstyle$h1)
     ),
   sidebarLayout( 
     sidebarPanel( width=6, #editor panel
@@ -33,129 +31,102 @@ shinyUI(fluidPage(
                              ),
                              navbarMenu("Code", 
                                         tabPanel("Edit Main" , value="Source")
-                                        #,
-                                        #tabPanel("Insert TagR"),
-                                        #tabPanel("Compounds"),
-                                        #tabPanel("Options")
                              ),
                              tabPanel(HTML("<li><a  href=\"http://mslegrand.github.io/svgR/User_Guide.html\"  target=\"_blank\" >User Guide </a></li>")),
                              tabPanel(HTML("<li><a  href=\"http://mslegrand.github.io/svgR/\"  target=\"_blank\" >io.svgR</a></li>"))
                   ),
-      style="background-color: #88B0CA; padding-top: 0px;", 
-
+      style=cstyle$sidePanel, 
       br(),br(),
       h3(textOutput( "fileName")),
       aceEditor( outputId = "source", value="", mode="r", theme="katzenmilch",
                  height = "650px", fontSize=16, autoComplete="live", 
                  autoCompleteList =list(svgR=names(svgR:::eleDefs))),
       actionButton("commit", label = "COMMIT EDIT", 
-                   style="background-color: #222244; color: white;
-                   border-radius: 24px; ")
+                   style=cstyle$button)
     ),
 #---------------------------------------------------------------
 #---------------plotNavBar  ------------------------------------
 # svgR plot panel
-    mainPanel( width=6, style="background-color: #88B0CA;", 
+    mainPanel( width=6, style=cstyle$wellPanel, 
       navbarPage("Plot Design:",  id="plotNavBar", fluid=TRUE, 
 #---------------plotNavBar:points  ------------------------------------
         tabPanel("Points", 
-                  wellPanel( 
-                         style="background-color: #8888AA; 
-                         border-color: #88AAAA; margin-top: 0px; 
-                         margin-bottom: 0px;",
-                    fluidRow(
-                      column(4, 
-                        selectInput(
-                          "ptSet", "Active Points",
-                          multiple=FALSE, size=3, selectize = FALSE,
-                          list("x"), selected="x", 
-                          width="150px"  
-                        )
-                      ),
-                      column(3,
-                        radioButtons("ptDisplayMode", 
-                          "Display Mode", 
-                          c("Normal","Labeled","Hidden"),
-                          width = "150px"
-                        )
-                      ),
-                      column(2,
-                             selectInput(
-                               "tagFreq", "Auto Tag",
-                               multiple=FALSE, size=1, selectize = FALSE,
-                               c(list("Off"),1:20), selected="Off", 
-                               width="80px"  
-                             )
-                     ),
-                     column(3,
-                            checkboxInput("insertMode",
-                                          "Insert Mode",
-                                          value = TRUE, width = "100px"
-                            )
-                     )
-                  ) 
+          wellPanel( 
+            style=cstyle$wellPoint,
+            fluidRow(
+              column(4, 
+                selectInput(
+                  "ptSet", "Active Points",
+                  multiple=FALSE, size=3, selectize = FALSE,
+                  list("x"), selected="x", 
+                  width="150px"  
                 )
-#             fixedRow( column(6, style = "background-color:yellow;", 
-#                                div(style = "height:50px;"))
-#             ),
-        ),
-
-
+              ),
+              column(3,
+                radioButtons("ptDisplayMode", 
+                  "Display Mode", 
+                  c("Normal","Labeled","Hidden"),
+                  width = "150px"
+                )
+              ),
+              column(2,
+                selectInput("tagFreq", "Auto Tag",
+                  multiple=FALSE, size=1, selectize = FALSE,
+                  c(list("Off"),1:20), selected="Off", 
+                  width="80px"  
+                )
+              ),
+              column(3,
+                checkboxInput("insertMode","Insert Mode",
+                  value = TRUE, width = "100px"
+                )
+              )
+            ) 
+          )#end of well panel
+        ), #end of tab panel "Points"
 #---------------plotNavBar:TAGS  ------------------------------------
         tabPanel("Tags",
           wellPanel( 
-            style="background-color: #8888AA; border-color: #88AAAA; 
-                  margin-top: 0px; margin-bottom: 0px;",
+            style=cstyle$wellPoint,
             fluidRow(
-             column(2, 
-                    selectInput(
-                      "tagPts", "Tagged Points",
-                      multiple=FALSE, size=3, selectize = FALSE,
-                      list(),  selected=NULL, 
-                      width="100px"  
-                    )
+              column(2, 
+                selectInput(
+                  "tagPts", "Tagged Points",
+                  multiple=FALSE, size=3, selectize = FALSE,
+                  list(),  selected=NULL, 
+                  width="100px"  
+                )
               ),
               column(2, 
-                    selectInput(
-                      "tagIndx", "Tag Index",
-                      multiple=FALSE, size=3, selectize = FALSE,
-                      list(), selected=NULL,
-                      width="60px"  
-                    )
+                selectInput("tagIndx", "Tag Index",
+                  multiple=FALSE, size=3, selectize = FALSE, list(), selected=NULL,
+                  width="60px"  
+                )
               ),
               column(2, 
-                    selectInput(
-                      "tagCol", "Column Name",
-                      multiple=FALSE, size=3, selectize = FALSE,
-                      list(),  selected=NULL, 
-                      width="100px"  
-                    )
+                selectInput("tagCol", "Column Name",
+                  multiple=FALSE, size=3, selectize = FALSE, list(),  selected=NULL, 
+                  width="100px"  
+                )
               ),
                      
               column(3, 
-                    selectInput(
-                      "tagColVal", "Column-Tag Value Choice",
-                      multiple=FALSE, size=3, selectize = FALSE,
-                      list(),  selected=NULL, 
-                      width="100px"  
-                    )
+                selectInput("tagColVal", "Column-Tag Value Choice", 
+                  multiple=FALSE, size=3, selectize = FALSE,  list(),  selected=NULL, 
+                  width="100px"  
+                )
               ),
-              column(3, textInput("tagValEd", "Alternate Value", 
-                                 value=""),
-                    actionButton("insertVal2Col", label = "Insert Val", 
-                                 style="background-color: #222244; color: white; border-radius: 24px; ")
-                    )
-              ),
-              style="background-color: #8888AA; border-color: #88AAAA; 
-                    margin-top: 0px; margin-bottom: 0px; margin-left:0; margin-right:0"
+              column(3, 
+                textInput("tagValEd", "Alternate Value", value=""),
+                actionButton("insertVal2Col", label = "Insert Val", style=cstyle$button)
+              )
+            ),
+            style=cstyle$wellPoint
           ) #well panel end
         ), #tab panel Tags end
 #---------------plotNavBar:Transform  ------------------------------------
-
         tabPanel("Transforms", 
-               style="background-color: #8888AA; 
-                       border-color: #88AAAA; margin-top: 0px; 
-                       margin-bottom: 0px;",
+               style=cstyle$wellPoint,
                tabsetPanel( id="transformOption",
                             tabPanel("Translate"), 
                             tabPanel("Rotate"), 
@@ -163,9 +134,11 @@ shinyUI(fluidPage(
                             type="pills"
                ) 
         ),
-
-        tabPanel("Log",
-         div( style="width:600px ;height: 640px; border: 1px solid darkblue; overflow: auto; background-color: white;",
+#---------------plotNavBar:Log  ------------------------------------
+        tabPanel("Log",br(),br(),
+         div( 
+           style=cstyle$log,
+             #"width:600px ;height: 640px; border: 1px solid darkblue; overflow: auto; background-color: white;",
               verbatimTextOutput("out_log")
          )
         )
@@ -173,29 +146,18 @@ shinyUI(fluidPage(
 #-----------plotNavBar:Plot--------------------------------------------
       conditionalPanel( "input.plotNavBar!='Log'",
         splitLayout(cellWidths = c("70%", "30%"),
-                    #h5('svgR plot', style="color: #4d3a7d; margin-top: 0px;margin-bottom: 0px;"),
                     checkboxInput("showGrid", "Show Coordinate Grid", value = TRUE, width = "200px")
         ),
-        div( style="width:600px ;height: 540px; border: 1px solid darkblue; overflow: auto; background-color: white;",
-             htmlOutput("svghtml")
-        )
+        div( style=cstyle$svg,htmlOutput("svghtml"))
       )
       ,br(),
 #-----------plotNavBar:Buttons--------------------------------------------
       conditionalPanel( "input.plotNavBar=='Points'",
         
-        actionButton("forwardPt", label = "Select Forward", 
-                  style="background-color: #222244; color: white;
-                  border-radius: 24px; "),
-        actionButton("backwardPt", label = "Select Back", 
-                  style="background-color: #222244; color: white;
-                  border-radius: 24px; "),
-        actionButton("removePt", label = "Selected Delete", 
-                     style="background-color: #222244; color: white;
-                  border-radius: 24px; "),
-        actionButton("tagPt", label = "Selected Tag", 
-                  style="background-color: #222244; color: white;
-                  border-radius: 24px; "),
+        actionButton("forwardPt", label = "Select Forward", style=cstyle$button),
+        actionButton("backwardPt", label = "Select Back", style=cstyle$button),
+        actionButton("removePt", label = "Selected Delete", style=cstyle$button),
+        actionButton("tagPt", label = "Selected Tag", style=cstyle$button),
         br(),br()
       )
     )
