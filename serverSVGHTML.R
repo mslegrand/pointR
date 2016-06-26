f1c5fdb41d7c5856a64d310e6a64952e3e50eccd
mark stephen legrand <coanalytics.net>
2016-06-26 13:52:01 -0400

    cleanup

--- serverSVGHTML.R
+++ serverSVGHTML.R
@@ -6,7 +6,7 @@ output$svghtml <- renderUI({
     return("")
   }
   
-  
+ # prep: pick script, get vbles 
   if(svgBarCmd=="Points"){
     ptName<-input$ptRSelect
     ptRList<-getPtDefs()$pt
@@ -24,7 +24,7 @@ output$svghtml <- renderUI({
     selectedPointIndx<-selectedPoint$point.index
     ptDisplayMode<-input$ptDisplayMode
     scriptName<-"Points"
-    #todo use input$pointOption :
+    # todo use input$pointOption :
     # pointOpt=c("Insert", "Edit","Tag")
     tag.indx<-as.numeric(input$tagIndx)
     showPtOptions<-list(ptDisplayMode=ptDisplayMode, tag.indx=tag.indx)
@@ -40,6 +40,10 @@ output$svghtml <- renderUI({
   src<-user$code
   src<-usingDraggable(src)
   
+  # called when we need to show points
+  # to do: rewrite to make this work with call for tags
+  # where each tag is a group, so that we can edit a tag set 
+  # to provide ability for translate, rotate, scale of points
   showPts %<c-% function(ptName,  showPtOptions=NULL){
     if(is.null(ptName) || is.null(showPtOptions) 
        || 
@@ -47,67 +51,68 @@ output$svghtml <- renderUI({
         is.null(showPtOptions$tag.indx))
        ){
       return(NULL)
-    }  
+    } 
+    ptRList<-getPtDefs()$pts #TODO: move this out!!!
+    pts<- ptRList[[ptName]]
+    if(length(pts)<2){
+      return(NULL)
+    }
+    tagRList<-getPtDefs()$df #TODO: move this out!!!
     tag.indx<-showPtOptions$tag.indx
  
     semitransparent<-0.3
     colorScheme<-c(default="green", ending="red", selected="blue")
-    ptRList<-getPtDefs()$pts #TODO: move this out!!!
-    pts<- ptRList[[ptName]]
-    tagRList<-getPtDefs()$df #TODO: move this out!!!
-    if(length(pts)<2){
-      return(NULL)
-    } else{
-      m<-matrix(pts,2)
-      if(!is.null(tagList) && 
-         !is.null(tag.indx) && 
-         !is.null(tagRList[[ptName]] 
-      )){
-        tags<-tagRList[[ptName]]$tag
-        ti<-which(tag.indx==tags)
-        tagInterval<-findInterval(sequence(ncol(m)),tags)
-        tagInterval<-tagInterval==ti
-        tagInterval[tagInterval==0]<-semitransparent
-        opac<-tagInterval
-        #selectedPointIndx<-0 #so tags will not show selectes
-      } else {
-        opac<-rep(1, ncol(m) )
-      }
-      lapply(1:ncol(m), function(i){
-        id<-paste("pd",ptName,i,sep="-")
-        pt<-m[,i]
-        color=colorScheme['default']
-        if(i==length(pts)/2) { #ncol(m)){
-            color=colorScheme['ending']   
-        }
-        list(
-          if(i==selectedPointIndx && is.null(tag.indx)){ #show selected point for points mode (or transform???)
-            circle(class="draggable", 
-                   id=id,  
-                   cxy=pt, r=9, fill="yellow", 
-                   opacity=opac[i],
-                   stroke=colorScheme['selected'], stroke.width=3,
-                   transform="matrix(1 0 0 1 0 0)", 
-                   onmousedown="selectPoint(evt)" )
-          } else { #a non-selected point
-            circle(class="draggable", 
-                   id=id,  
-                   cxy=pt, r=8, fill=color, opacity=opac[i],
-                   transform="matrix(1 0 0 1 0 0)", 
-                   onmousedown="selectPoint(evt)" )
-          }
-          ,
-          
-          if(showPtOptions$ptDisplayMode=="Labeled"){
-            text(paste(i), cxy=pt+10*c(1,-1),  
-                 stroke='black', font.size=12, opacity=1) #opac)
-          } else {
-            NULL
-          }
-        )
-      })
+    m<-matrix(pts,2)
+     
+    #preproc for tagList 
+    if(!is.null(tagList) && 
+       !is.null(tag.indx) && 
+       !is.null(tagRList[[ptName]] 
+    )){
+      tags<-tagRList[[ptName]]$tag
+      ti<-which(tag.indx==tags)
+      tagInterval<-findInterval(sequence(ncol(m)),tags)
+      tagInterval<-tagInterval==ti
+      tagInterval[tagInterval==0]<-semitransparent
+      opac<-tagInterval
+      #selectedPointIndx<-0 #so tags will not show selectes
+    } else {
+      opac<-rep(1, ncol(m) )
     }
-  }
+    
+    #form list of  all point renderings
+    lapply(1:ncol(m), function(i){
+      id<-paste("pd",ptName,i,sep="-")
+      pt<-m[,i]
+      color=colorScheme['default']
+      if(i==length(pts)/2) { #ncol(m)){
+          color=colorScheme['ending']   
+      }
+      list(
+        if(i==selectedPointIndx && is.null(tag.indx)){ #show selected point for points mode (or transform???)
+          circle(class="draggable", 
+                 id=id,  
+                 cxy=pt, r=9, fill="yellow", 
+                 opacity=opac[i],
+                 stroke=colorScheme['selected'], stroke.width=3,
+                 transform="matrix(1 0 0 1 0 0)", 
+                 onmousedown="selectPoint(evt)" )
+        } else { #a non-selected point
+          circle(class="draggable", 
+                 id=id,  
+                 cxy=pt, r=8, fill=color, opacity=opac[i],
+                 transform="matrix(1 0 0 1 0 0)", 
+                 onmousedown="selectPoint(evt)" )
+        },
+        if(showPtOptions$ptDisplayMode=="Labeled"){
+          text(paste(i), cxy=pt+10*c(1,-1),  
+               stroke='black', font.size=12, opacity=1) #opac)
+        } else {
+          NULL
+        }
+      )
+    }) #end lapply
+  } #end showPts
   
   newPtLayer %<c-% function(svgBarCmd, wh=c(1200,800)){
     if(svgBarCmd=="Points" && input$insertMode==TRUE){
