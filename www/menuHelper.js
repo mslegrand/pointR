7edba2d99dcaba8225939c8636b818c238c13838
sup <legrand@coanalytics.net>
2016-09-13 12:51:37 -0400

    helper for menu diabling

---
+++ www/menuHelper.js
@@ -0,0 +1,35 @@
+
+shinyjs.disableTab = function(name) {
+var tab = $('.nav li a[data-value=' + name + ']');
+tab.bind('click.tab', function(e) {
+e.preventDefault();
+return false;
+});
+tab.addClass('disabled');
+}
+
+shinyjs.enableTab = function(name) {
+var tab = $('.nav li a[data-value=' + name + ']');
+tab.unbind('click.tab');
+tab.removeClass('disabled');
+}
+
+shinyjs.disableMenu = function(navBarId, pos) {
+//var men =$('#' + navBarId +' > :nth-child(2)' )
+var men =$('#plotNavBar li:nth-child(2)' )
+//var men =$('#' + navBarId +'children()[1].child())' )
+men.addClass('disabled');
+$('#plotNavBar li:nth-child(2) a' ).css('color','grey');
+men.bind('click.dropdown-menu', function(e) {
+e.preventDefault();
+return false;
+});
+}
+
+shinyjs.enableMenu = function(navBarId, pos) {
+var men =$('#plotNavBar li:nth-child(2)' )
+$('#plotNavBar li:nth-child(2) a' ).css('color', '#333388');
+men.unbind('click.dropdown-menu');
+men.removeClass('disabled');
+}
+
