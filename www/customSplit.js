9b26723b6de31b4b2d9f65036581bf692361e6d7
sup <legrand@coanalytics.net>
2016-11-26 10:47:29 -0500

    Resizable right panel (appears to be working)

--- www/customSplit.js
+++ www/customSplit.js
@@ -1,4 +1,24 @@
+  var resizeStuff=function(){
+   $("#mySplitter").css("height", parseInt($(parent.window).height()));
+   $("#mySplitter").css("width", parseInt($(parent.window).width()));
+   $("#mySplitter").trigger("resize"); 
+  };
+
+
 $().ready(function(){
-  $("#MySplitter").splitter();
+  $("#mySplitter").splitter();
+  $("#mySplitter").css("height", $(window).innerHeight()-5).trigger("resize");
+
+  $(window).resize(function() {
+      resizeStuff();
+  }).resize();
+       
+  $(window).trigger("resize");
+  
 });
+
+//$().splitter({splitbarKey: "I", anchorToWindow: true, resizeToWidth: true});
+
+
 $().splitter({splitbarKey: "I"});
+
