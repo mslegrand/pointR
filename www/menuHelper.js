c32ec4c259b8715fac1146bd45a04b1190ac97d8
sup <legrand@coanalytics.net>
2016-12-15 12:33:00 -0500

    remove commented out legacy

--- www/menuHelper.js
+++ www/menuHelper.js
@@ -14,24 +14,6 @@ tab.unbind('click.tab');
 tab.removeClass('disabled');
 };
 
-//shinyjs.disableMenu = function(name) {
-//var men =$(name );
-//men.addClass('disabled');
-//var tmp = name+' a';
-//$(tmp ).css('color','grey');
-//men.bind('click.dropdown-toggle', function(e) {
-//e.preventDefault();
-//return false;
-//});
-//};
-
-//shinyjs.enableMenu = function(name) {
-//var men =$(name);
-//var tmp = name+' a';
-//$(tmp ).css('color','#333388');
-//men.unbind('click.dropdown-menu');
-//men.removeClass('disabled');
-//};
 
 shinyjs.ignoreClick=function(e) {
   e.preventDefault();
