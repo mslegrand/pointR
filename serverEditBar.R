77914e76370ac3fd4337a7e99c2de91cee1a3e3c
sup <legrand@coanalytics.net>
2016-12-15 12:34:16 -0500

    themes, font size, indent

--- serverEditBar.R
+++ serverEditBar.R
@@ -73,8 +73,111 @@ observeEvent( input$editNavBar, {
     }
     updateNavbarPage(session, "editNavBar", selected ="tab1")
   }
+  if(fileCmd=="Theme"){
+    themes<-getAceThemes()
+    modalTheme <- function() {
+      modalDialog(
+        selectInput("selectTheme", "Theme", themes, multiple=FALSE, 
+                    selectize = FALSE, width="300px", size=1  ), 
+        footer = tagList(actionButton("modalThemeCancel", "Cancel"),actionButton("modalThemeOk", "OK") )
+      ) 
+    }
+    showModal( modalTheme() )
+  } 
+  if(fileCmd=="Font Size"){
+    fontsSizes<-6:36
+    modalFontSize <- function() {
+      modalDialog(
+        selectInput("selectFontSize", "Select Font Size", fontsSizes, multiple=FALSE, 
+                    selectize = FALSE, width="90px", size=1  ), 
+        footer = tagList(actionButton("modalFontSizeCancel", "Cancel"),actionButton("modalFontSizeOk", "OK") )
+      ) 
+    }
+    showModal( modalFontSize() )
+  }
+  if(fileCmd=="Indentation"){
+    indentSizes<-1:12
+    modalIndentSize <- function() {
+      modalDialog(
+        selectInput("IndentSize", "Select Indent", indentSizes, multiple=FALSE, 
+                    selectize = FALSE, width="90px", size=1  ), 
+        footer = tagList(actionButton("modalIndentSizeCancel", "Cancel"),actionButton("modalIndentSizeOk", "OK") )
+      ) 
+    }
+    showModal( modalIndentSize() )
+  }
   
+}) 
+
+#- editor options handlers
+
+observeEvent(input$modalIndentSizeCancel, {
+  updateNavbarPage(session, "editNavBar", selected ="tab1")
+  removeModal()
+}) 
+
+observeEvent(input$modalIndentSizeOk, {
+  nextSize<-input$selectIndentSize
+  session$sendCustomMessage(type = "shinyAceExt",  
+        list(id= "source", tabSize=as.numeric(nextSize))) 
+  updateNavbarPage(session, "editNavBar", selected ="tab1")  
+  removeModal()
 })
+
+observeEvent(input$modalFontSizeCancel, {
+  updateNavbarPage(session, "editNavBar", selected ="tab1")
+  removeModal()
+}) 
+
+observeEvent(input$modalFontSizeOk, {
+  #nextSize<-select.list(choices = 6:24, graphics=TRUE)
+  nextSize<-input$selectFontSize
+  updateAceEditor(session, "source", fontSize=as.numeric(nextSize) )
+  updateNavbarPage(session, "editNavBar", selected ="tab1")  
+  removeModal()
+})
+
+observeEvent(input$modalThemeCancel, {
+  updateNavbarPage(session, "editNavBar", selected ="tab1")
+  removeModal()
+}) 
+
+observeEvent(input$modalThemeOk, {
+  nextTheme<-input$selectTheme
+  if(nextTheme!=""){
+    updateAceEditor(session, "source", theme=nextTheme) 
+  } 
+  updateNavbarPage(session, "editNavBar", selected ="tab1")
+  
+  isDark<-function(nt){
+    nt %in% c(
+      "ambiance","chaos","clouds_midnight","cobalt","idle_fingers",
+      "kr_theme","merbivore","merbivore_soft","mono_industrial","monokai",
+      "pastel_on_dark","solarized_dark","terminal","tomorrow_night","tomorrow_night_blue",
+      "tomorrow_night_bright","tomorrow_night_eighties","twilight","vibrant_ink"
+    )
+  }
+  colorSet<-list(
+    ".ace_svgRAN"=c(255,140,0), ".ace_svgRME"=c(112,128,144),
+    ".ace_svgRFE" =c(0,255,255), ".ace_svgRGR" =c(255,0,255),
+    ".ace_svgRSH" =c(0,0,205), ".ace_svgRCO" =c(172,61,239),
+    ".ace_svgRTX" =c(178,34,34), ".ace_svgRFI" =c(0,128,0),
+    ".ace_svgRMM" =c(240,82,45) )
+  
+  colorSet<-lapply(colorSet, function(col){
+    if(isDark(nextTheme)){
+      col<-pmin(255,col+160)
+    }
+    paste0("rgb(", paste0(col,collapse=","), ")")
+  })
+
+  session$sendCustomMessage(
+    type = "shinyAceExt",    
+    list(id= "source", resetElementColor=colorSet)
+  ) 
+  removeModal()
+})
+
 
 #------fileName-------------
 output$fileName <- renderText({ 
