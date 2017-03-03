61d30ed6f3fc5837bbbaac664f883fe832c994f7
sup <legrand@coanalytics.net>
2017-03-03 13:35:58 -0500

    snippets to load

---
+++ inst/App/blank2.R
@@ -0,0 +1,18 @@
+#svgR elements: font-face glyph missing-glyph hkern vkern font font-face-name font-face-format font-face-uri animate animateColor animateMotion animateTransform set mpath feFuncA feFuncB feFuncG feFuncR feDistantLight feTurbulence feConvolveMatrix feDiffuseLighting feOffset filter feBlend feColorMatrix feComponentTransfer feComposite feDisplacementMap feFlood feGaussianBlur feImage feMerge feMorphology feSpecularLighting feTile feSpotLight fePointLight svg a altGlyph circle clipPath cursor defs ellipse foreignObject g image line linearGradient marker mask path pattern polygon polyline radialGradient rect script switch symbol text textPath tref tspan use view altGlyphDef altGlyphItem color-profile desc feMergeNode font-face-src glyphRef metadata stop style title font.face missing.glyph font.face.name font.face.format font.face.uri color.profile font.face.src translate rotate rotatR scale u.em u.ex u.px u.pt u.pc u.cm u.mm u.in u.prct u.rad
+library(svgR)
+WH<-c(1500,1500)
+
+#Defined by mouse: edit with care!
+ptR<-list(
+  x=c()
+)
+
+svgR(wh=WH, 
+#your custom code goes here
+
+  NULL
+
+
+
+)
+
