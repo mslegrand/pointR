f119a1a3d45e6b031d8ff030c56fd73a61c03149
m. s. legrand <slegrand3.14@gmail.com>
2017-10-04 15:43:41 -0400

    cleanup comment

--- inst/App/leftPanel/cmdFileSave.R
+++ inst/App/leftPanel/cmdFileSave.R
@@ -1,11 +1,7 @@
 cmdFileSave<-function(){
-  # curDir<-getCurrentDir()
-  # curFile<-getCurrentFile()
-  # if(nchar(curDir)>0 && 
-  #    nchar(curFile)>0 
-  # )
+  
   if(getFileNameStatus()==TRUE){
-    #fullFilePath<-paste(curDir, curFile, sep="/")
+    
     fullFilePath<-getCurrentFilePath()
     isolate({
       editOption$.saved<-TRUE
