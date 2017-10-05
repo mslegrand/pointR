f8e34106550f320bae0dabe2672eec3f03ffee8b
m. s. legrand <slegrand3.14@gmail.com>
2017-10-05 18:31:10 -0400

    added shinyAce to resourcePatn

--- R/init.R
+++ R/init.R
@@ -4,7 +4,12 @@ initResourcePaths <- function() {
   if (is.null(.global$loaded)) {
     shiny::addResourcePath(
       prefix = 'pointR',
-      directoryPath = system.file('www', package='pointR'))
+      directoryPath = system.file('www', package='pointR')
+    )
+    shiny::addResourcePath(
+      prefix = 'shinyAce',
+      directoryPath = system.file('www', package='shinyAce')
+    )
      .global$loaded <- TRUE
   }
   HTML("")
