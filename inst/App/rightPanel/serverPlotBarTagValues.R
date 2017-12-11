b9c0fbe921f0e69c84433b51e5339b1e8ca2a9c6
m. s. legrand <slegrand3.14@gmail.com>
2017-12-11 12:23:25 -0500

    implemented add column

--- inst/App/rightPanel/serverPlotBarTagValues.R
+++ inst/App/rightPanel/serverPlotBarTagValues.R
@@ -105,22 +105,55 @@ observeEvent(returnValue4ModuleTagVal$deleteColumn(),{
 
 
 #--------NEW COLUMN------------------------------
+attrValueModal <- function(errMssg=NULL) {
+  doOk<-"shinyjs.triggerButtonOnEnter(event,\"commitNewCol\")"
+  modalDialog(
+    onkeypress=doOk, 
+    span('Enter both a name for the new column and a value for its entries'), 
+    textInput("modalAttrName", "Enter the name for the new column"),
+    textInput("modalAttrValue", "Enter an entry value for the new column"), 
+    if(!is.null(errMssg)){
+      div(tags$b(errMssg, style = "color: red;"))
+    },
+    footer = tagList(
+      modalButton("Cancel"),
+      actionButton("commitNewCol", "Commit")
+    )
+  ) 
+}
+
 observeEvent(returnValue4ModuleTagVal$newColumn(),{
-  if(rightPanel()=='tagValues'){
-    # name<-returnValue4ModuleTagVal$name()
-    # rowIndex<-returnValue4ModuleTagVal$rowIndex()
-    # if(!is.null(name)){
-    #   newTib<-getPtDefs()$tib[[name]]
-    #   matColIndex<-ncol(newTib[[rowIndex, getTibPtColPos()]])
-    #   pts<-newTib[[getTibPtColPos()]]
-    #   point.index<-ptPos2AbsPtIndx(pts, rowIndex, matColIndex)
-    #   
-    #   updateSelected(name=name, row=rowIndex, 
-    #                  matCol=matColIndex, 
-    #                  point.index=point.index)
-    # }
+  if(identical( rightPanel(), 'tagValues')){
+     showModal( attrValueModal() )
   }
 })
+
+observeEvent(input$commitNewCol, {
+  if(identical( rightPanel(), 'tagValues')){
+    #checks 
+    if(!grepl(pattern = "^[[:alpha:]]", input$modalAttrName)){ # check name syntax
+      showModal(attrValueModal( errMssg="Invalid Column Name: must begin with a character") )
+    } else if( input$modalAttrName %in% names(getTib()) ){ # check name uniqueness
+      showModal(attrValueModal( errMssg="Invalid Column Name: this name is already taken!") )
+    } else if(!grepl(pattern = "^[[:graph:]]", input$modalAttrValue) ){  # check value uniqueness
+      showModal(attrValueModal( errMssg="Invalid Column Value: must begin with printable character other than space") )
+    } else { 
+      #add name to tib
+      newPtDefs<-getPtDefs()
+      newColName<-input$modalAttrName
+      
+      newPtDefs$tib[[getTibName()]]<-add_column(newPtDefs$tib[[getTibName()]], 
+                                                !!(newColName):=input$modalAttrValue   )     
+      # updateAce
+      
+      sender<-'addNewColumn'
+      updateAceExtDef(newPtDefs, sender=sender)
+      # set selection to this column?
+      removeModal() #close dialog
+    }
+  }
+})
+
 
 
 # modalEditValue<-function( oldValue, row, columnName){
@@ -157,7 +190,6 @@ observeEvent(returnValue4ModuleTagVal$entryValue(),{
        nchar(returnValue4ModuleTagVal$entryValue())>0 &&
        (getTibColumn()!=getTibPtColPos() 
     )  ){
-      
       entry<-returnValue4ModuleTagVal$entryValue()
       # !!! TODO: type check if numeric
       #cat('entry=',entry,'\n')
@@ -170,22 +202,6 @@ observeEvent(returnValue4ModuleTagVal$entryValue(),{
       newPtDefs$tib[[getTibName()]][[row,column ]]<-entry
       updateAceExtDef(newPtDefs, sender=sender)
     }
-    
-    
-    
-    # name<-returnValue4ModuleTagVal$name()
-    # rowIndex<-returnValue4ModuleTagVal$rowIndex()
-    # rowIndex<-returnValue4ModuleTagVal$rowIndex()
-    # if(!is.null(name)){
-    #   newTib<-getPtDefs()$tib[[name]]
-    #   matColIndex<-ncol(newTib[[rowIndex, getTibPtColPos()]])
-    #   pts<-newTib[[getTibPtColPos()]]
-    #   point.index<-ptPos2AbsPtIndx(pts, rowIndex, matColIndex)
-    #   
-    #   updateSelected(name=name, row=rowIndex, 
-    #                  matCol=matColIndex, 
-    #                  point.index=point.index)
-    # }
   }
 })
 
