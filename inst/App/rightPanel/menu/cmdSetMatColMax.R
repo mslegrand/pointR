d4edfe8db2ef8df6559e035a825a7e49fe8d406e
m. s. legrand <slegrand3.14@gmail.com>
2018-02-24 15:49:03 -0500

    reintegrated maxCol option

--- inst/App/rightPanel/menu/cmdSetMatColMax.R
+++ inst/App/rightPanel/menu/cmdSetMatColMax.R
@@ -1,11 +1,19 @@
 
 setMatColMaxModal <- function(columnName, currentValue=1) {
-    title<-paste0('Henceforth,  for the column "',columnName,'" the number of points in each row are to be limited by the following.')
+    title<-paste0(
+      'Henceforth,  for the column "',
+      columnName,
+      '" the number of points in each row are to be limited by the following.'
+    )
+    
     modalDialog(
       span(title),
-      numericInput( "tibMatColMax", 'max', currentValue, min=1, step=1, 
-                    width= '70px' ),
-      checkboxInput('limitTibMatCol', 'Apply to limit the no. of columns (points) in matrix',value = !is.null(currentValue) ),
+      numericInput( "tibMatColMax", 'max', currentValue, min=1, step=1,  width= '70px' ),
+      checkboxInput(
+        'limitTibMatCol', 
+        'Apply to limit the no. of columns (points) in matrix',
+        value = !is.null(currentValue) 
+      ),
       footer = tagList(
         modalButton("Cancel"),
         actionButton("setMatColMaxModalOk", "OK") 
@@ -15,7 +23,6 @@ setMatColMaxModal <- function(columnName, currentValue=1) {
   
 observeEvent(input$setMatColMaxModalOk, {
   if(!is.null(input$tibMatColMax) && !is.na(input$tibMatColMax)){
-     #setHandlerValue(input$tibMatColMax)
      updateWidgetChoicesRow(maxVal= input$tibMatColMax )
   } else {
     updateWidgetChoicesRow(maxVal= NULL )
