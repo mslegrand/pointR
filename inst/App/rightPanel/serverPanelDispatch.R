a22ab10cb3a270db8749f19a98333bbbf718ce69
m. s. legrand <slegrand3.14@gmail.com>
2018-03-06 00:00:12 -0500

    split panelCoordinator into panelCoordinator and panelDispatch

---
+++ inst/App/rightPanel/serverPanelDispatch.R
@@ -0,0 +1,34 @@
+
+output$TopRightPanel<-renderUI({
+  moduleEdTibUI("tagValBar", input, output)
+})
+
+output$LeftMidRightPanel<-renderUI({
+  moduleRowDNDUI("rowDND", input, output)
+})
+
+output$BottomRightPanel<-renderUI({
+  moduleFooterRightUI("footerRight", input, output)
+})
+
+output$MidRightPanel<-renderUI({
+  
+  chosenRightMidPanel<-getRightMidPanel2()
+  cat("chosenRightMidPanel=",format(chosenRightMidPanel),"\n")
+  if (chosenRightMidPanel=='point'){
+    modulePlotSVGrUI("svgPointsMod")
+  } else if (chosenRightMidPanel=='value'){
+    modulePlotSVGrUI("svgTagValsMod")
+  } else if (chosenRightMidPanel=='matrix'){
+    modulePlotSVGrUI("svgTagDragMod")
+  } else if (chosenRightMidPanel == transformTag ){
+    modulePlotSVGrUI("svgTransformMod")
+  } else if( chosenRightMidPanel == svgPanelTag ){
+    modulePlotSVGrUI("svgPointsMod")
+  } else if (chosenRightMidPanel %in% c(errorPanelTag, RPanelTag)){
+    moduleLogUI("errLogMod")
+  }
+  
+  
+  
+})
