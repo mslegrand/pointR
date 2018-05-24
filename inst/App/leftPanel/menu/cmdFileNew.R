b0d0533c3d6ee623fffba38df487b6fab5d3d473
mslegrand <coanalytics.net>
2018-05-24 10:46:43 -0400

    added function cmdFileNewRmd

--- inst/App/leftPanel/menu/cmdFileNew.R
+++ inst/App/leftPanel/menu/cmdFileNew.R
@@ -9,8 +9,6 @@ cmdFileNew<-function(){
   
   tabName<-getNextAnonymousFileName()
   
-  
-   
   #aceId<-tabName2AceId(tabId)
   addFileTab(title=tabName, txt=codeTemplate,  docFilePath="?")
   #triggerRefresh('cmd.commit', rollBack=FALSE)
@@ -30,7 +28,37 @@ cmdFileNew<-function(){
   # )
   mssg$error<-""
 
- 
   
+}
+
+
+cmdFileNewRmd<-function(){
+  src<-rmdTemplate
+  
+  # session$sendCustomMessage(
+  #   type = "shinyAceExt",
+  #   list(id= getAceEditorId(), sender='cmd.file.new', setValue= src, ok=TRUE)
+  # )
+  
+  tabName<-getNextAnonymousFileName()
+  
+  #aceId<-tabName2AceId(tabId)
+  addFileTab(title=tabName, txt=src,  docFilePath="?", mode='markdown')
+  #triggerRefresh('cmd.commit', rollBack=FALSE)
+  #setSourceType(sourceType=svgPanelTag)
+  #cat("sendCustomMessage sender='cmd.file.new' aceId=",aceId,"\n" )
+  #updateSelected(columnName='x') #kludge for getting ensuring asset selection will be x
+  delay(500, 
+        session$sendCustomMessage(
+          type = "shinyAceExt",
+          list(id=  getAceEditorId(), sender='cmd.file.new', setValue= src, setDocFileSaved=TRUE, ok=TRUE)
+        ) 
+  )
+  
+  # session$sendCustomMessage(
+  #   type = "shinyAceExt", 
+  #   list(id= getAceEditorId(), setValue=src, sender='cmd.file.new' )
+  # )
+  mssg$error<-""
   
 }
