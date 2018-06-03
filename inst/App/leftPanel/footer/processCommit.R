29dc75c6f945a68ddce2283fcf4105c9d3471ffa
m. s. legrand <slegrand3.14@gmail.com>
2018-06-03 16:06:57 -0400

    svg output now also goes to RPanel

--- inst/App/leftPanel/footer/processCommit.R
+++ inst/App/leftPanel/footer/processCommit.R
@@ -38,66 +38,59 @@ processCommit<-reactive({
       }
       
       if(length(svgRPos)==0){ # just R code I guess
+        #test for error and capture output
         # capture capture output as mssg
         env<-new.env()
-        
-        #output<-c(">")
-        #tryCatch({
         output<-lapply(lines, function(line){
-            captureOutput(eval(parse(text=line), envir=env))
+          captureOutput(eval(parse(text=line), envir=env))
         })
-        #}
-        #,
-        # error=function(e){ 
-        #   e<-c(e,traceback())
-        # }
-        #)
         output<-paste( output, collapse="\n" )
         output<-paste("Output:",output,sep="\n")
-       
-        setSourceType(sourceType=RPanelTag) #no error, just R code
         setCapturedMssg(output)
-        #base::stop(output , call.=FALSE, domain=NA);
+        setSourceType(sourceType=RPanelTag) #no error, just R code
       } else { # presume to be svgR code
-        setSourceType(sourceType=svgPanelTag) #SVG code
         # next check if it can be run
         parsedCode<-parse(text=src) 
-        eval(parsedCode)
-      
-      # passed so far
- 
-      # no error occured so all systems go!!!!
-      #remove all removeAllMarkers from ace since all sys go.
-      updateAceExt( id= getAceEditorId(), sender='commit.removeMarkers', removeAllMarkers='removeAllMarkers', setOk=TRUE)
+        output<-captureOutput(eval(parsedCode))
+        output<-paste( output, collapse="\n" )
+        output<-paste("Output:",output,sep="\n")
+        setCapturedMssg(output)
+        setSourceType(sourceType=svgPanelTag) #SVG code
       }
+        # passed so far
+   
+        # no error occured so all systems go!!!!
+        #remove all removeAllMarkers from ace since all sys go.
+        updateAceExt( id= getAceEditorId(), sender='commit.removeMarkers', removeAllMarkers='removeAllMarkers', setOk=TRUE)
     }, #end of try
     error=function(e){ 
-      #Error handler for commit
-      if(all(!str_detect(e,'Output:'))){
-        e<-c(e,traceback())
-      }
-      err<-paste(unlist(e), collapse="\n", sep="\n")
-      #try to locate where the error occured
-      if(str_detect(err, 'parse')){
-        m<-str_match(err, ":([0-9]+):([0-9]+):")
-        if(length(m)==3){
-          row=as.numeric(m[2])-1
-          col=as.numeric(m[3])-1
-          updateAceExt(id= getAceEditorId(), addMarker=c(row,col), sender='commit.addmarker' )
+        #Error handler for commit
+        if(all(!str_detect(e,'Output:'))){
+          e<-c(e,traceback())
         }
-      }
-      if(str_detect(err,' not found')){
-        m<-str_match(err, "object '([^']+)' not found")
-        if(length(m)==2){ #find the first line where this occurs
-          notFound<-paste0("\\b",m[2],"\\b")
-          srcs<-str_split(src,"\n")[[1]]
-          row<-min(which(str_detect(srcs,notFound)))-1
-          col<-1
-          updateAceExt(id= getAceEditorId(), addMarker=c(row,col), sender='commit.addmarker' )
+        err<-paste(unlist(e), collapse="\n", sep="\n")
+        #try to locate where the error occured
+        if(str_detect(err, 'parse')){
+          m<-str_match(err, ":([0-9]+):([0-9]+):")
+          if(length(m)==3){
+            row=as.numeric(m[2])-1
+            col=as.numeric(m[3])-1
+            updateAceExt(id= getAceEditorId(), addMarker=c(row,col), sender='commit.addmarker' )
+          }
         }
-      }
-      setErrorMssg(err)
-    }) 
-  }
+        if(str_detect(err,' not found')){
+          m<-str_match(err, "object '([^']+)' not found")
+          if(length(m)==2){ #find the first line where this occurs
+            notFound<-paste0("\\b",m[2],"\\b")
+            srcs<-str_split(src,"\n")[[1]]
+            row<-min(which(str_detect(srcs,notFound)))-1
+            col<-1
+            updateAceExt(id= getAceEditorId(), addMarker=c(row,col), sender='commit.addmarker' )
+          }
+        }
+        setErrorMssg(err)
+      } #end of error handler
+    ) #end of tryCatch 
+  } #end of if(length==1)
 })
 
