f3f3ff5a68667d0c59b1bf73a4f01fcadf6ea45c
m. s. legrand <slegrand3.14@gmail.com>
2018-06-06 12:57:33 -0400

    update about, version,  license

--- inst/App/leftPanel/menu/cmdAbout.R
+++ inst/App/leftPanel/menu/cmdAbout.R
@@ -6,13 +6,14 @@ cmdAbout<-function(){
 
 modalAbout <- function(..., size = "m" ) {
   modalDialog(
+    div( img(src="ptR/pointRLogo.SVG")),
     div( width="100%",
       p("SVG is short for scalar vector graphics."),
       p("The purpose of this application is to provide an open-flexible tool for creating SVG using the svgR package."),
       p("The svgR package is an R package to for creating SVG graphics."),
       p("Applications include Shiny apps, Rdocs, or even generating a plain SVG file."),
-      p("This software is licensed under the MIT License"),
-      p("Copyright (c) 2016 M. S. Legrand. All rights reserved")
+      p("This software is licensed under the GPL-3 License"),
+      p("Copyright (c) 2018 M. S. Legrand. All rights reserved")
     ),
     title="About",
     footer=tagList( modalButton("Dismiss")),
