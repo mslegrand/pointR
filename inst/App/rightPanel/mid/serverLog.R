c318269b7ab5f7fa6e5cb227fc07f32985bc828f
m. s. legrand <slegrand3.14@gmail.com>
2018-06-18 10:50:03 -0400

    split moduleLog into seperate file

--- inst/App/rightPanel/mid/serverLog.R
+++ inst/App/rightPanel/mid/serverLog.R
@@ -5,34 +5,6 @@
 
 
 
-moduleLogUI <- function(id, input, output) { 
-  ns <- NS(id)
-  absolutePanel( 
-    draggable=FALSE,
-    "class"="cLogText",
-    verbatimTextOutput(ns("out_log"))
-  )
-}
-
-moduleLog<-function(
-  input, output, session, 
-  id,
-  panelName,
-  logMssgs
-){
-  output$out_log<-renderText({ 
-    
-    if(panelName() %in% c( errorPanelTag, RPanelTag)){
-      txt<-logMssgs()
-      if(length(txt)==0){
-        txt=""
-      }
-      txt
-    }
-    
-  })
-}
-
 errLogModuleList<-callModule( #auto  input, output, session 
   module=moduleLog, 
   id="errLogMod", 
