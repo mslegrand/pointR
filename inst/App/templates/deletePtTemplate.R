8360e303d1307dc96f9b8dc5c0d99a8bca509c90
m. s. legrand <slegrand3.14@gmail.com>
2018-07-03 15:13:05 -0400

    changed template to include just a code block

--- inst/App/templates/deletePtTemplate.R
+++ inst/App/templates/deletePtTemplate.R
@@ -1,7 +1,9 @@
 
-onDeletePoint<-function(tibs, tibName, tibColumn, rowIndex, ptColIndex){
-  m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
-  m<-deletePt(m, ptColIndex) #internal convenience function
-  tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
-  tibs # 
-}
+tibs<-deletePt(location=getLocation(), tibs=getTibs() )
+
+# onDeletePoint<-function(tibs, tibName, tibColumn, rowIndex, ptColIndex){
+#   m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
+#   m<-deletePt(m, ptColIndex) #internal convenience function
+#   tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
+#   tibs # 
+# }
--- inst/App/templates/newPtTemplate.R
+++ inst/App/templates/newPtTemplate.R
@@ -1,7 +1,12 @@
 
-onNewPoint<-function(tibs, tibName, tibColumn, pt, rowIndex, ptColIndex){
-  m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
-  m<-insertPt(m, pt, ptColIndex) #internal convenience function
-  tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
-  tibs # 
-}
+pt<-getPt() # coordinates derived from the mouse click
+tibs<-insertPt(pt=pt, location=getLocation(), tibs=getTibs() )
+#tibs assigned for insertion
+
+# onNewPoint<-function(tibs, tibName, tibColumn, pt, rowIndex, ptColIndex){
+#   m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
+#   m<-insertPt(m, pt, ptColIndex) #internal convenience function
+#   tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
+#   tibs # 
+# }
+# 
