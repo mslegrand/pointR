f5fcfe5c8d808dcb7232644f2fd88255a10b93eb
m. s. legrand <slegrand3.14@gmail.com>
2018-07-03 15:14:03 -0400

    changed to use just code block

--- inst/App/templates/movePtTemplate.R
+++ inst/App/templates/movePtTemplate.R
@@ -1,7 +1,11 @@
 
-onMovePoint<-function(tibs, tibName, tibColumn, pt, rowIndex, ptColIndex){
-  m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
-  m<-movePt(m, pt, ptColIndex) #internal convenience function
-  tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
-  tibs # 
-}
+
+pt<-getPt() # coordinates derived from the mouse click
+tibs<-movePt(pt=pt, location=getLocation(), tibs=getTibs() )
+
+# onMovePoint<-function(tibs, tibName, tibColumn, pt, rowIndex, ptColIndex){
+#   m<-tibs[[tibName]][[tibColumn]][[rowIndex]]
+#   m<-movePt(m, pt, ptColIndex) #internal convenience function
+#   tibs[[tibName]][[tibColumn]][[rowIndex]]<-m
+#   tibs # 
+# }
