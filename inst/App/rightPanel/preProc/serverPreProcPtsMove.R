49c9e8107e5bb5dde31ab44db935dffc451daed1
m. s. legrand <slegrand3.14@gmail.com>
2018-07-04 23:04:16 -0400

    prepoints adde

---
+++ inst/App/rightPanel/preProc/serverPreProcPtsMove.R
@@ -0,0 +1,27 @@
+
+
+movePoint<-function(pt, location=getLocation()  ){
+  tibs<-location$tibs
+  
+  if( !is.null(tibs) &&
+      is_scalar_character(location$assetName) && 
+      location$assetName %in% names(tibs) 
+  ){
+    
+    tib<-tibs[[ location$assetName ]]
+    rowIndex<-location$rowIndex
+    if( 
+      is_scalar_numeric(location$rowIndex) &&
+      is_scalar_numeric(location$matColIndex)  &&
+      rowIndex>0 && 
+      rowIndex<=nrow(tib)
+    ){
+      columIndex<-getTibPtColPos() #which(names(tib)==ptColName)
+      tib[[rowIndex,columIndex]][,location$matColIndex] <-pt
+      tibs[[location$assetName]]<-tib
+    }
+  }
+  tibs
+}
+
+
