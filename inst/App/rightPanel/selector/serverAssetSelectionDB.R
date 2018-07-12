20d530c1f1472a705ada9023b8d78aac2e2e71ba
m. s. legrand <slegrand3.14@gmail.com>
2018-07-12 14:36:58 -0400

    cleanup

--- inst/App/rightPanel/selector/serverAssetSelectionDB.R
+++ inst/App/rightPanel/selector/serverAssetSelectionDB.R
@@ -9,9 +9,6 @@ serverAssetDB<-reactiveValues(
     ptColName="NULL",   # !!! KLUDGE for now. should this default to last col?
     selIndex=1,         # only used when current col is points, 
     transformType='Translate',
-    # ptAddScript="NULL",
-    # ptMoveScript="NULL",
-    # ptDeleteScript="NULL",
     ptScriptSel=preprocChoices[1]
   )[0,]
 )
@@ -36,9 +33,7 @@ restoreAssetState<-function(nextTabId){
   }
   if(!is.null(row.tib)){
     lapply(names(row.tib), function(n){
-      # cat('n=',format(n),'\n')
       v<-row.tib[[n]][1]
-      # cat('v=',format(v),'\n')
       if(is.na(v)){
         v<-NULL
       } 
