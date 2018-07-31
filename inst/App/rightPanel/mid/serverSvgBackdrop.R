124117de3a5eac0488089edab0d745f7ec96f528
m. s. legrand <slegrand3.14@gmail.com>
2018-07-31 17:01:19 -0400

    backdropDB to monitor backdrop on a perpage basis

--- inst/App/rightPanel/mid/serverSvgBackdrop.R
+++ inst/App/rightPanel/mid/serverSvgBackdrop.R
@@ -1,28 +1,61 @@
 #--Backdrop-----------------------------------
 
-backDrop<-reactiveValues(
-  color='white',
-  checked=TRUE
+backDropDB<-reactiveVal(
+  tibble(
+    tabId='bogus',
+    color='white',
+    checked=TRUE
+  )
 )
 
-observeEvent(input$solidBackdrop,{
-  backDrop$checked=!input$solidBackdrop
-})
-
-observeEvent(input$backdropColour,{
-  backDrop$color=input$backdropColour
-}, ignoreNULL = TRUE)
-
-getBackDrop<-reactive({
-  list(color=backDrop$color, checked=backDrop$checked)
-})
-
-
-setBackDrop<-function(hide, color){
-  if(!missing(hide)){
-    backDrop$checked<-!hide
-  }
-  if(color){
-    backDrop$color<-color
+setBackDrop<-function(pageId, checked, color){
+  if(!is.null(pageId)){
+    tb<-backDropDB()
+    tt<-as.list(filter(tb, tabId==pageId))
+    if(length(tt$tabId)==0){
+      tt<-list(
+        tabId=pageId,
+        color='white',
+        checked=FALSE
+      )
+    }
+    if(!missing(color)){ tt$color=color}
+    if(!missing(checked)){tt$checked=checked}
+    tb<-filter(tb,tabId!=pageId)
+    tb<-bind_rows(tb,tt)
+    backDropDB(tb)
   }
 }
+
+
+observeEvent(input$pages,{
+  bd<-getBackDrop()
+  updateColourInput(session , inputId="backdropColour", value=bd$color)
+  updateCheckboxInput(session, inputId="solidBackdrop", value=!bd$checked)
+})
+
+observeEvent(input$solidBackdrop,{
+  checked<-!input$solidBackdrop
+  setBackDrop(pageId=input$pages, checked=checked)
+}, ignoreNULL=TRUE)
+
+observeEvent(input$backdropColour,{
+  color=input$backdropColour
+  setBackDrop(pageId=input$pages, color=color)
+}, ignoreNULL = TRUE)
+
+
+getPageBackDrop<-function(pageId){
+  tb<-backDropDB()
+  if(!is.null(pageId) && pageId %in% tb$tabId){
+    list(color=tb[tb$tabId==pageId,]$color, checked=tb[tb$tabId==pageId,]$checked )
+  } else {
+    list(color='white', checked=FALSE)
+  }  
+}
+
+getBackDrop<-reactive({
+  getPageBackDrop(input$pages)
+})
+
+
