7bfb0ec077c96d1cf49482c6c02922eeddb74661
m. s. legrand <slegrand3.14@gmail.com>
2018-08-06 14:40:15 -0400

    clean up

--- inst/App/fileIO/serverLoadWorkSpace.R
+++ inst/App/fileIO/serverLoadWorkSpace.R
@@ -3,12 +3,12 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
   cat('>---> restoreWorkSpace\n')
   wsPages<-list()
   fileWSPaths<-dir(workSpaceDir, pattern='PTR-TABID', full.names = T)
-  cat('#1 restoreWorkSpace\n')
+
   if(length(fileWSPaths)==0){
     return(FALSE)
   }
-  cat('#2 restoreWorkSpace\n')
-  # browser()
+  
+  
   # 1. load all pages into a list.
   for(filePath in fileWSPaths){
     page<-readRDS(filePath)
@@ -21,8 +21,7 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
     # 2 remove filePath file
     file.remove(filePath)
     newFilePath<-paste0(workSpaceDir,"/",id,".rda")
-    # newFilePath<-paste0(id,".rda")
-    # cat(newFilePath,"\n")
+    
     saveRDS(object=page, file = newFilePath)
   }
   cat('#3 restoreWorkSpace\n')
@@ -37,7 +36,7 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
     tib<-bind_rows(tib, tibAs)
   }
   serverAssetDB$tib<-tib
-  cat('#4 restoreWorkSpace\n')
+  
   # --- backDropDB
   tib<-backDropDB()
   pattern<-"^backdrop."
@@ -48,7 +47,7 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
     tib<-bind_rows(tib, tibAs)
   }
   backDropDB(tib)
-  cat('#5 restoreWorkSpace\n')
+  
   # ---grid---
   tib<-svgGridDB()
   pattern<-"^grid."
@@ -59,9 +58,7 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
     tib<-bind_rows(tib, tibAs)
   }
   svgGridDB(tib)
-  # cat("svgGRid::\n")
-  # print(svgGridDB())
-  cat('#6 restoreWorkSpace\n')
+  
   #--- tribbleDB
   tib<-useTribbleFormatDB()
   pattern<-"^trib."
@@ -73,24 +70,16 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
   }
   useTribbleFormatDB(tib)
   
-  cat('#7 restoreWorkSpace\n')
   #--- dnippetsDB
   tib<-dnippetsDB$usage
   pattern<-"^dnip."
   for(page in wsPages){
     tibAs<-page[ grep(pattern, names(page)) ]
-    #browser()
-    # for(n in names(tibAs)){
-    #   cat("tibAs name=", format(n), "\n")
-    #   cat("tibAs value=", format(tibAs[n]), "\n")
-    # }
     names(tibAs)<-gsub(pattern, '', names(tibAs))
-    #tibAs<-as.tibble(tibAs)
     tib<-bind_rows(tib, tibAs)
   }
   dnippetsDB$usage<-tib
   
-  cat('#8 restoreWorkSpace\n')
   # --- preProcDB
   tib<-preProcDB$points
   for(page in wsPages){
@@ -106,7 +95,6 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
   }
   preProcDB$points<-tib
   
-  cat('#9 restoreWorkSpace\n')
   # ------- fileDescDB
   tib<-fileDescDB()
   for(page in wsPages){
@@ -122,36 +110,34 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
   }
   fileDescDB(tib)
   
-  cat('#10 restoreWorkSpace\n')
-  
   for(page in wsPages){
     # extract the serverAsset portion and add
     tabId=page$fileDescriptor.tabId
     
     aceId<-tabID2aceID(tabId)
-    cat('page:: tabId=',format(tabId), 'aceId=',format(aceId),"\n" )
+    
     mode=page$fileDescriptor.mode
-    cat('page:: tabId=',format(tabId), 'mode=',format(mode),"\n" )
+    
     docFilePath=page$fileDescriptor.filePath
-    cat('page:: tabId=',format(tabId), 'docFilePath=',format(docFilePath),"\n" )
+    
     fileSaveStatus=page$fileDescriptor.isSaved 
-    cat('page:: tabId=',format(tabId), 'isSaved=',fileSaveStatus,"\n" )
+    
     txt=page$code
-    cat('#11 restoreWorkSpace\n')
+    
     if(!identical(docFilePath, "?")){
       title=basename(docFilePath)
     } else {
       title=paste('Anonymous', page$fileDescriptor.anonNo)
     }
     #missing the addFileDesc
-    cat('#12 restoreWorkSpace\n')
+    
     if(mode=='ptr'){
       divClass="cAceContainer"
     } else {
       divClass="cAceRmdContainer"
     }
     #addFileDesc(pageId=tabId, docFilePath=docFilePath, fileSaveStatus, fileMode=mode)
-    cat('#13 restoreWorkSpace\n')
+    
     appendTab(
       inputId = "pages",
       tabPanel( #tabId,
@@ -177,21 +163,20 @@ restoreWorkSpace<-function( workSpaceDir=getWorkSpaceDir() ){
         value=tabId
       )
     )
-    cat('#14 restoreWorkSpace\n')
+    
     restoreAssetState(tabId)
     #updateAceExt(id=aceId, sender='cmd.tabChange', roleBack=FALSE, setfocus=TRUE, getValue=TRUE)
     cat('#15 restoreWorkSpace\n')
-    
     updateTabsetPanel(session, inputId='pages', selected=tabId)
-    
+    cat('#16 restoreWorkSpace\n')
   }
   delay(500,{
     for(page in wsPages){
-      cat('#16 restoreWorkSpace\n')
+      
       tabId=page$fileDescriptor.tabId
       fileSaveStatus=page$fileDescriptor.isSaved 
       savedStatus<-ifelse(fileSaveStatus, 'saved', 'notSaved')
-      cat('#17 restoreWorkSpace\n')
+      
       if(is.null(tabId)){ browser() }
       addNewPage2dnippetsDB(tabId)
       cat('#18 restoreWorkSpace\n')
--- inst/App/leftPanel/dnippets/serverDnippetCntrl.R
+++ inst/App/leftPanel/dnippets/serverDnippetCntrl.R
@@ -1,16 +1,10 @@
 dnippetSelection<-reactiveValues(
-  #current=NULL, # contains names of selected dnippets
   all=list() # contains all dnippets 
 )
 
-# addDrippets<-function(dnName, dnippets, select=dnName ){
-#   drippetSelection$all[[dnName]]=dnippets
-#   #drippetSelection$current<-c(drippetSelection$current, select)
-# }
 
 add2DnippetsSelectionAll<-function(dnName, dnippets ){
   dnippetSelection$all[[dnName]]=dnippets
-  #drippetSelection$current<-c(drippetSelection$current, select)
 }
 
 
@@ -18,13 +12,8 @@ add2DnippetsSelectionAll<-function(dnName, dnippets ){
 observeEvent(c( getDnippetsAll(), dnippetsDB$usage, input$pages),{
   if(!is.null(input$pages)){
     mode<-getMode()
-    cat("\n>-----> observeEvent(c( getDnippetsAll(), getDnippetsSelected(), input$pages):\n"  )
+    # cat("\n>-----> observeEvent(c( getDnippetsAll(), getDnippetsSelected(), input$pages):\n"  )
     all<-getDnippetsAll()
-    cat("getDnippetsAll()=",format(all),"\n")
-    cat("getDnippetsSelected()=",format(getDnippetsSelected()),"\n")
-    cat("input$pages=",format(input$pages),"\n")
-    cat("Now updateAwesomeCheckboxGroup with inputId= selectedDDDnippets\n")
-    # browser()
     updateAwesomeCheckboxGroup(session, 
        inputId="selectedDDDnippets", 
        choices  = getDnippetsAll(),
@@ -35,7 +24,7 @@ observeEvent(c( getDnippetsAll(), dnippetsDB$usage, input$pages),{
     selected<-getDnippetsSelected()
     dnippets<-dnippetSelection$all[selected]
     dnippets<-unlist(dnippets,recursive=F)
-    cat("sendPtRManagerMessage cmd.dnippet.file.load\n")
+    # cat("sendPtRManagerMessage cmd.dnippet.file.load\n")
     names(dnippets)<-NULL
     if(length(dnippets)==0){
       sendPtRManagerMessage(sender='cmd.dnippet.file.load', removeDrippets=runif(1));
@@ -49,30 +38,19 @@ observeEvent(c( getDnippetsAll(), dnippetsDB$usage, input$pages),{
   } else {
     hideElement('selectedDnippetButtonBoxContainer')
   }
-  cat("<-----< observeEvent(c( getDnippetsAll(), getDnippetsSelected(), input$pages):\n\n"  )
+  # cat("<-----< observeEvent(c( getDnippetsAll(), getDnippetsSelected(), input$pages):\n\n"  )
 })
 
 observeEvent(input$selectedDDDnippets,{
   if(!is.null(input$pages)){
     
-    cat("\n>---> observeEvent(input$selectedDDDnippets\n")
-    cat('input$pages=',format(input$pages),"\n")
+    # cat("\n>---> observeEvent(input$selectedDDDnippets\n")
+    
     selected=input$selectedDDDnippets
-    cat("selected=",format(selected),"\n")
-    cat("about to setDnippetsSelected(",format(input$pages), ",", selected,")")
-    # browser()
     setDnippetsSelected(input$pages , selected)
-    #drippetSelection$selected=selected
-    cat("<---<  observeEvent(input$selectedDDDnippets\n\n")
+    # cat("<---<  observeEvent(input$selectedDDDnippets\n\n")
   }
   
 }, ignoreInit = FALSE, ignoreNULL = FALSE)
 
-# observeEvent( input$drippetSelectionDropdown, {
-#   #tabId=input$pages
-#   updateAwesomeCheckboxGroup(session,
-#                              inputId="drippetSelectionDropdown",
-#                              choices  = getDnippetsAll(),
-#                              selected = getDnippetsSelected(),
-#                              inline = FALSE, status = "primary")
-# })
+
