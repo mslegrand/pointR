8459962b532b74132403d6b46e1cc45fb4454040
m. s. legrand <slegrand3.14@gmail.com>
2018-08-23 21:34:06 -0400

    removing NA in datapath

--- inst/App/leftPanel/menu/cmdFileOpenProject.R
+++ inst/App/leftPanel/menu/cmdFileOpenProject.R
@@ -9,6 +9,7 @@ observeEvent(input$buttonFileOpenProject,{
   fp.dt<-parseFilePaths(c(wd='~'), input$buttonFileOpenProject)
   if(length(fp.dt)>0 && nrow(fp.dt)){
     datapath<-as.character(fp.dt$datapath[1])
+    datapath<-gsub(pattern = '^NA/', "~/", datapath)
     pathToProj<-dirname(datapath)
     projName<-basename(datapath)
     openProj(projName, pathToProj)
