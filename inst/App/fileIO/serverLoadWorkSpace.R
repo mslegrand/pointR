f04882bbee77da24cbffc5dc410d316023075a75
m. s. legrand <slegrand3.14@gmail.com>
2018-11-14 00:58:42 -0500

    discard dead code

--- inst/App/fileIO/serverLoadWorkSpace.R
+++ inst/App/fileIO/serverLoadWorkSpace.R
@@ -20,186 +20,11 @@ restoreWorkSpace<-reactive({
   for(filePath in fileWSPaths){
     page<-readRDS(filePath)
     # A. assign tabIds to each page
-    # id=getNextTabId()
     id=basename(filePath)
-    # # substitute value ending in tabId with id
-    # tbid<-grep("tabId$", names(page))
-    # page[tbid]<-id
     wsPages[[id]]<-page
   } 
-  # load project.pprj
- 
-  # if(length(pprjPath)>0){
-  #   pprj<-read_json(pprjPath)
-  #   if(!identical(pprj$pathToProj, getWorkSpaceDir())){
-  #     for(page in wsPages){
-  #       fp<-page$fileDescriptor.filePath
-  #       page$fileDescriptor.filePath<-gsub(getWorkSpaceDir(), pprj$pathToProj, fp)
-  #     }
-  #   }
-  # }
-  # # 2 remove filePath file
-  # for(filePath in fileWSPaths){
-  #   file.remove(filePath)
-  # }
-  # 
-  # # 3 write pages
-  # for(id in names(wsPages)){
-  #   newFilePath<-paste0(workSpaceDir,"/",id,".rda")
-  #   page<-wsPages[[id]]
-  #   saveRDS(object=page, file = newFilePath)
-  # }
   
-  #4. iterate through pages and add to serverAssetDB$tib
-  
-  # # --- serverAssetDB
-  # tib<-NULL #serverAssetDB$tib
-  # pattern<-"^assetSelection."
-  # for(page in wsPages){
-  #   tibAs<-page[ grep(pattern, names(page)) ]
-  #   names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #   tibAs[sapply(tibAs,is.null)]<-NA
-  #   tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #     # if(inherits( tib, 'tbl_df')){ 
-  #     #   cat('glimpse\n')
-  #     #   glimpse(tib )
-  #     #   cat("ncol(tib)=",ncol(tib),"\n")
-  #     #   #cat(names('names(tib)=', paste(names(tib),collapse=","), "\n"))
-  #     #   cat("nrow(tib)=",nrow(tib),"\n")
-  #     # } else { print(format(tib))}
-  #     # cat("class(tibAs) =" , class(tibAs), "\n")
-  #     # print('tibAs=')
-  #     # print(format(tibAs))
-  #     # cat("names(tibAs)=",names(tibAs),"\n")
-  #     e<-c(e,"\n", traceback())
-  #     stop(e)
-  #   })
-  # }
-  # serverAssetDB$tib<-tib
-  # 
-  # # --- backDropDB
-  # tib<-backDropDB()
-  # pattern<-"^backdrop."
-  # for(page in wsPages){
-  #   tibAs<-page[ grep(pattern, names(page)) ]
-  #   names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #   tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #     print(pattern)
-  #     print(format(tib))
-  #     print(format(tibAs))
-  #     e<-c(e,traceback())
-  #     stop(e)
-  #   })
-  # }
-  # backDropDB(tib)
-  # 
-  # # ---grid---
-  # tib<-svgGridDB()
-  # pattern<-"^grid."
-  # for(page in wsPages){
-  #   tibAs<-page[ grep(pattern, names(page)) ]
-  #   names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #   tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #     print(pattern)
-  #     print(format(tib))
-  #     print(format(tibAs))
-  #     e<-c(e,traceback())
-  #     stop(e)
-  #   })
-  # }
-  # svgGridDB(tib)
-  # 
-  # #--- tribbleDB
-  # tib<-useTribbleFormatDB()
-  # pattern<-"^trib."
-  # for(page in wsPages){
-  #   tibAs<-page[ grep(pattern, names(page)) ]
-  #   names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #   tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #     print(pattern)
-  #     print(format(tib))
-  #     print(format(tibAs))
-  #     e<-c(e,traceback())
-  #     stop(e)
-  #   })
-  # }
-  # useTribbleFormatDB(tib)
-  # 
-  # #--- dnippetsDB
-  # tib<-dnippetsDB$usage
-  # pattern<-"^dnip."
-  # for(page in wsPages){
-  #   tibAs<-page[ grep(pattern, names(page)) ]
-  #   names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #   tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #     print(pattern)
-  #     print(format(tib))
-  #     print(format(tibAs))
-  #     e<-c(e,traceback())
-  #     stop(e)
-  #   })
-  # }
-  # dnippetsDB$usage<-tib
-  # 
-  # # --- preProcDB
-  # tib<-preProcDB$points
-  # pattern<-"^preprocScripts."
-  # for(page in wsPages){
-  #   # extract the serverAsset portion and add
-  #   asi<-grep(pattern, names(page))
-  #   if(length(asi)>0){
-  #     tibAs<-page[asi]
-  #     tn<-gsub(pattern, '', names(tibAs))
-  #     names(tibAs)<-tn
-  #     tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #       print(pattern)
-  #       print(format(tib))
-  #       print(format(tibAs))
-  #       e<-c(e,traceback())
-  #       stop(e)
-  #     })
-  #   }
-  # }
-  # preProcDB$points<-tib
-  # 
-  # # ------- fileDescDB
-  # tib<-fileDescDB()
-  # pattern<-"^fileDescriptor."
-  # for(page in wsPages){
-  #   # extract the serverAsset portion and add
-  #   asi<-grep(pattern, names(page))
-  #   if(length(asi)>0){
-  #     tibAs<-page[asi]
-  #     tn<-gsub(pattern, '', names(tibAs))
-  #     names(tibAs)<-tn
-  #     tryCatch({tib<-bind_rows(tib, tibAs)}, error=function(e){
-  #       print(pattern)
-  #       print(format(tib))
-  #       print(format(tibAs))
-  #       e<-c(e,"\n",traceback())
-  #       stop(e)
-  #     })
-  #   }
-  # }
-  # fileDescDB(tib)
-  
-  
-  # extractDBFromPages<-function(wsPages, pattern, initTib){
-  #   for(page in wsPages){
-  #     tibAs<-page[ grep(pattern, names(page)) ]
-  #     if(length(tibAs)>0){
-  #       names(tibAs)<-gsub(pattern, '', names(tibAs))
-  #       tibAs[sapply(tibAs,is.null)]<-NA
-  #       tryCatch({initTib<-bind_rows(initTib, tibAs)}, error=function(e){
-  #         stop('failure with pattern=',pattern, "and page=\n")
-  #       })
-  #     }
-  #   }
-  #   return(initTib)
-  # } 
-  
-  
-  extractDBFromPages<-function(wsPages, pattern, initTib){
+   extractDBFromPages<-function(wsPages, pattern, initTib){
     rtv<-lapply(wsPages, function(page){
       tibAs<-page[ grep(pattern, names(page)) ]
       if(length(tibAs)>0){
@@ -212,10 +37,7 @@ restoreWorkSpace<-reactive({
     rtv
   }  
   
-  
 
-  
-  
   tabId<-'bogus'
   mode<-'ptr'
   txt<-NULL
@@ -266,62 +88,11 @@ restoreWorkSpace<-reactive({
         value=tabId
       )
     )
-    # if(!(tabId %in% serverAssetDB$tib$tabId) ){
-    #   browser()
-    #   stop('tabId=', tabId, 'not found in serverAssetDB$tib$tabId')
-    # }
     
-    # delay(500,{
-    #   updateAceExt(id=  aceId, sender='cmd.file.new', mode=mode, getValue= TRUE,  ok=TRUE )
-    # }
-    #)
-    
-    # cat('here we are!!!!\n')
-    # cat('tabId=',format(tabId),"\n")
-    # xyz<-input$pages
-    # cat('input$pages=',format(xyz),"\n")
-    #restoreAssetState(tabId)
   }
-  
-  # delay(3000,
-  #       { updateTabsetPanel(session, inputId='pages', selected=tabId)})
-  
-   # browser()
-  
-  # aceId=tabID2aceID(tabId)
-  # cat('aceId=',format(aceId),'\n')  
-
-  # 
-  # cat('code=',format(txt),"\n")
-  # request$code<-txt
-   # updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
-  # updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
-  
-  
-  
-  
-  # dt=500 # delay
-  # delay(500,{
-  #   for(page in wsPages){
-  #     tabId=page$fileDescriptor.tabId
-  #     cat('page in wsPages:: tabId=',tabId,"\n")
-  #     fileSaveStatus=page$fileDescriptor.isSaved 
-  #     savedStatus<-ifelse(fileSaveStatus, 'saved', 'notSaved')
-  #     
-  #     if(is.null(tabId)){ browser() }
-  #     #addNewPage2dnippetsDB(tabId)
-  #     sendFileTabsMessage(resize=runif(1), tabId=tabId, savedStatus= savedStatus)
-  #     aceId=tabID2aceID(tabId)
-  #     updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
-  #   }
-  # })
-  
-  #browser()
-  # delay(3000,{
   
   serverAssetDB$tib<-extractDBFromPages(wsPages, "^assetSelection.", initTib=initialServerAssetDB() )
 
-  # browser()
   tib<-extractDBFromPages(wsPages, "^backdrop.", initTib=initialBackDropDB() )
   backDropDB(tib)
 
@@ -341,36 +112,7 @@ restoreWorkSpace<-reactive({
   cat("tabId=",tabId,"\n")
   cat('input$pages=',format(input$pages),"\n")
   
-  # delay(5000,{
-  #   cat('2 input$pages=',format(input$pages),"\n")
-  #   restoreAssetState(tabId)
-  #   reOrgPanels(id=tabID2aceID(tabId), mode= mode )
-  #   processCommit()
-  # })
-  
-  # 
-  #Sys.sleep(3)
-  
-  
-    
-    #updateAceExt(id=  tabID2aceID(tabId), sender='cmd.file.new', mode=mode, getValue= TRUE,  ok=TRUE )
-  
-  
-  
-    #updateAceExt(id=  aceId, sender='cmd.file.new', mode=mode, getValue= TRUE,  ok=TRUE )
-  #})
-  
-  # })
 
-  
-
-  # dt2<-(1+length(wsPages))*dt
-  # dt2<-2000
-  # delay(dt2,{
-  #   updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
-  # })
-  
-  #updateAceExt(id=  getAceEditorId(), sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
   cat('<---< restoreWorkSpace\n')
   return(aceId)
 })
