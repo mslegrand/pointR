1b234db5ba7472147c8abecc552a33911bef2450
m. s. legrand <slegrand3.14@gmail.com>
2019-02-01 11:47:16 -0500

    cleaning

--- inst/App/util/utilTibble.R
+++ inst/App/util/utilTibble.R
@@ -1,30 +1,30 @@
+# # sole caller: extractSafeRowColIndex (below)
+# extractColumnIndex<-function(tib, colName){
+#   if(!is.null(colName) && !is.null(tib)){
+#     #if(!("tbl_df" %in% class(tib))){ stop("extractColumnIndex: tib is not a tibble")}
+#     match(colName, names(tib), nomatch=NULL)
+#   } else {
+#     NULL
+#   }
+# }
+# 
+# # !!! not called
+# extractSafeRowColIndex<-function(tib, rowIndex, colName){
+#   if(missing(tib) || missing(rowIndex) || missing(colName)){ stop("extractColumnIndex: missing param")}
+#   if(is.null(tib)){ return(NULL)}
+#   #if(!('tibble' %in% class(tib))){ stop("extractColumnIndex: tib is not a tibble")}
+#   if(is.null(colName)){return(NULL)}
+#   if(is.null(rowIndex)){ return(NULL) }
+#   if(rowIndex<1 || rowIndex>nrow(tib)){return(NULL)}
+#   colIndex<-match(colName, names(tib), nomatch=NULL)
+#   if(is.null(colIndex)){ return(NULL)}
+#   list(rowIndex=rowIndex, colIndex=colIndex)
+# }
+# 
 
-extractColumnIndex<-function(tib, colName){
-  if(!is.null(colName) && !is.null(tib)){
-    #if(!("tbl_df" %in% class(tib))){ stop("extractColumnIndex: tib is not a tibble")}
-    match(colName, names(tib), nomatch=NULL)
-  } else {
-    NULL
-  }
-}
 
-extractSafeRowColIndex<-function(tib, rowIndex, colName){
-  if(missing(tib) || missing(rowIndex) || missing(colName)){ stop("extractColumnIndex: missing param")}
-  if(is.null(tib)){ return(NULL)}
-  #if(!('tibble' %in% class(tib))){ stop("extractColumnIndex: tib is not a tibble")}
-  if(is.null(colName)){return(NULL)}
-  if(is.null(rowIndex)){ return(NULL) }
-  if(rowIndex<1 || rowIndex>nrow(tib)){return(NULL)}
-  colIndex<-match(colName, names(tib), nomatch=NULL)
-  if(is.null(colIndex)){ return(NULL)}
-  list(rowIndex=rowIndex, colIndex=colIndex)
-}
-
-
-
-
+# sole caller: observeEvent( returnValue4ModuleRtFtr$tagPt() in serverFooterRight.R
 # y=util to discover which colunns might be points
-# used in serverFooterRight.R
 tagTib<-function(tib, ptColIndex,  rowIndex=nrow(tib), matCol){
   # cat('-----------------inside tagTib-------------')
   tmp<-bind_rows(tib[1:rowIndex,], tib[rowIndex:nrow(tib),])
