42b2b908588c594d3d79cd529d57669c8ee67150
m. s. legrand <slegrand3.14@gmail.com>
2019-04-06 18:46:18 -0400

    update save status upon input

--- inst/App/www/Acejs/ptRaceInit.js
+++ inst/App/www/Acejs/ptRaceInit.js
@@ -3,6 +3,8 @@
         ace.UndoManager.call(this);
         this.$ok=[];
         this.$initiallySaved=false;
+        //this.$savedState;
+        this.$wasSaved=false;
       }
       //ud.$ok=[];
       UMD.prototype=Object.create(ace.UndoManager.prototype);
@@ -26,11 +28,13 @@
           return this.isClean();
         }
       };
+      
      UMD.prototype.setSaved = function(){ 
         if(this.$undoStack.length === 0){
           this.$initiallySaved=true;
         } else {
           this.markClean();
+          //this.$savedState=true;
         }
       };      
       
@@ -87,13 +91,11 @@ function ptRaceInit(data){
   //console.log("ptRaceInit:: data.initSaved=" +JSON.stringify(data.initSaved)); 
   var mode = data.mode;
   var $el = $('#' + id);
-  //console.log($el.attr());
-  //console.log($el.attr());
-  //console.log('JSON.stringify($el.data)')
+
   var theEditor = $el.data('aceEditor'); 
   var autoComplete = data.autoComplete[0];
   var autoCompleteList=data.autoCompleteList;
-  //console.log('autoCompleteList=', JSON.stringify(autoCompleteList));
+
   
   if(!!theEditor){
     console.log('found theEditor');
@@ -110,142 +112,169 @@ function ptRaceInit(data){
   $el.data('errorMarkerArray', []);
   
   if(!!data.docFilePath){
-    //console.log('setting  docFilePath=' + data.docFilePath);
     $el.data('docFilePath', data.docFilePath);
   }
   
-    if(mode == 'ptr' || mode=='ptrrmd' || mode=='dnippets'){ 
-      data.acejs= data.acejs[0];
-      ace.config.set('workerPath', "./Acejs");
-      ace.config.set('modePath', "./Acejs");
-    }  
-      theEditor.getSession().setMode('ace/mode/' + mode);  // shinyAce init
-      theEditor.getSession().setOption('useWorker', true);
-      theEditor.getSession().setUseSoftTabs(true);
-      theEditor.getSession().setUseWrapMode(false);
-      theEditor.renderer.setShowGutter(true);
-      theEditor.setHighlightActiveLine(true);
+  if(mode == 'ptr' || mode=='ptrrmd' || mode=='dnippets'){ 
+    data.acejs= data.acejs[0];
+    ace.config.set('workerPath', "./Acejs");
+    ace.config.set('modePath', "./Acejs");
+  }  
+  theEditor.getSession().setMode('ace/mode/' + mode);  // shinyAce init
+  theEditor.getSession().setOption('useWorker', true);
+  theEditor.getSession().setUseSoftTabs(true);
+  theEditor.getSession().setUseWrapMode(false);
+  theEditor.renderer.setShowGutter(true);
+  theEditor.setHighlightActiveLine(true);
       
-      if(autoComplete == "live") {
-        theEditor.setOption('enableLiveAutocompletion', true);
-      }
-      if(autoComplete != "disabled") {
-        theEditor.setOption('enableBasicAutocompletion', true);
-        theEditor.setOption('enableSnippets', true);
-      }
-      if(autoComplete!="disabled") {
-        theEditor.completers.splice(1, 1); // to get rid of the ducpliate local matches 
-        //console.log(JSON.stringify(theEditor.completers.length));
-        //console.log(JSON.stringify(theEditor.completers));
-      }
-      if (data.hasOwnProperty('autoCompleteList')){
-          $el.data('autoCompleteList', data.autoCompleteList);
-      }
+  if(autoComplete == "live") {
+    theEditor.setOption('enableLiveAutocompletion', true);
+  }
+  if(autoComplete != "disabled") {
+    theEditor.setOption('enableBasicAutocompletion', true);
+    theEditor.setOption('enableSnippets', true);
+  }
+  if(autoComplete!="disabled") {
+    theEditor.completers.splice(1, 1); // to get rid of the ducpliate local matches 
+    //console.log(JSON.stringify(theEditor.completers.length));
+    //console.log(JSON.stringify(theEditor.completers));
+  }
+  if (data.hasOwnProperty('autoCompleteList')){
+      $el.data('autoCompleteList', data.autoCompleteList);
+  }
       //Next a custom history manager
       
       
-      var ud = new UMD(); 
-      if(!!data.initSaved){
-          ud.$initiallySaved=data.initSaved;
-          console.log('---->undo manager .$initiallySaved=' + ud.$initiallySaved);
-      }      
-      //console.log('initial undo :' + JSON.stringify(ud));
-      theEditor.getSession().setUndoManager(ud);
+  var ud = new UMD(); 
+  if(!!data.initSaved){
+      ud.$initiallySaved=true; //data.initSaved;
+      console.log('---->undo manager .$initiallySaved=' + ud.$initiallySaved);
+  }      
+  
+  theEditor.getSession().setUndoManager(ud);
       
-      theEditor.commands.addCommand({
-        name: 'commitSource',
-        bindKey: {win: 'Ctrl-Shift-Enter', mac: 'Command-Shift-Enter'},
-        exec: function(editor) {
-            var randomString = function(length) {
-                var text = '';
-                var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                for(var i = 0; i < length; i++) {
-                    text += possible.charAt(Math.floor(Math.random() * possible.length));
-              }
-              return text;
-            };
-            Shiny.onInputChange('commitMssg', randomString(5) );
-        }
-      });
+  theEditor.commands.addCommand({
+    name: 'commitSource',
+    bindKey: {win: 'Ctrl-Shift-Enter', mac: 'Command-Shift-Enter'},
+    exec: function(editor) {
+        var randomString = function(length) {
+            var text = '';
+            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for(var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+          }
+          return text;
+        };
+        Shiny.onInputChange('commitMssg', randomString(5) );
+    }
+  });
       
-      theEditor.commands.addCommand({
-        name: 'closeWindow',
-        bindKey: {win: 'Ctrl-x', mac: 'Command-x'},
-        exec: function(editor) {
-            event.stopPropagation();
-            var randomString = function(length) {
-                var text = '';
-                var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-                for(var i = 0; i < length; i++) {
-                    text += possible.charAt(Math.floor(Math.random() * possible.length));
-              }
-              return text;
-            };
-            Shiny.onInputChange('closeTab',  { id :id,  type:'aceId', rnd: randomString(5) });
-        }
-      });
+  theEditor.commands.addCommand({
+    name: 'closeWindow',
+    bindKey: {win: 'Ctrl-x', mac: 'Command-x'},
+    exec: function(editor) {
+        event.stopPropagation();
+        var randomString = function(length) {
+            var text = '';
+            var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+            for(var i = 0; i < length; i++) {
+                text += possible.charAt(Math.floor(Math.random() * possible.length));
+          }
+          return text;
+        };
+        Shiny.onInputChange('closeTab',  { id :id,  type:'aceId', rnd: randomString(5) });
+    }
+  });
       
       
   
-      theEditor.commands.addCommand({
-        name: 'helpR',
-        bindKey: {win: 'F1', mac: 'F1'},
-        exec: function(editor) {
-          console.log('--------------helpR!----------------------');
-          editor.getSession().selection.moveCursorLongWordLeft();
-          editor.getSession().selection.selectWordRight();
-          var text = editor.getSession().getTextRange(editor.getSelectionRange());
-          Shiny.onInputChange('helpMssg', {query:text, num:Math.random(), editorId: id} );
-        } //TODO: remove editorVar; appears not to be used
-      });
-      
-      theEditor.on("guttermousedown", function(e) {
-       // derived from the following link
-       //https://stackoverflow.com/questions/16864236/how-to-mark-line-numbers-in-javascript-ace-editor
-        var target = e.domEvent.target; 
-        if (target.className.indexOf("ace_gutter-cell") == -1)
-            return; 
-        if (!theEditor.isFocused()) 
-            return; 
-        if (e.clientX > 25 + target.getBoundingClientRect().left) 
-            return; 
-        var breakpoints = e.editor.session.getBreakpoints(row, 0);
-        var row = e.getDocumentPosition().row;
-        if(typeof breakpoints[row] === typeof undefined)
-            e.editor.session.setBreakpoint(row);
-        else
-            e.editor.session.clearBreakpoint(row);
-        e.stop();
-     });
- 
-    
-    $el.droppable({
-
-    activeClass: "ui-state-default",
-    hoverClass: "ui-state-hover",
-    accept: ":not(.ui-sortable-helper)",
-
-    drop: function(event, ui) {
-      var pos = $el.data('aceEditor').renderer.screenToTextCoordinates(event.clientX, event.clientY);
-      
-      console.log("pos=" + JSON.stringify(pos));
-      console.log("ui =\n" + JSON.stringify(ui));
-      var txt =  ui.draggable.attr("data-snippet");
-      console.log(
-        'txt=' + JSON.stringify(txt)
-      );
-      $el.data('aceEditor').moveCursorToPosition(pos);
-      $el.data('aceEditor').focus();
-	    var tab_press= jQuery.Event('keydown', {which: 88});
-       
-      console.log('drop occurred');
- 	    $el.trigger(tab_press);
-       var snippetManager = ace.require("ace/snippets").snippetManager;
-      snippetManager.insertSnippet($el.data('aceEditor'), txt);
-
-      return true;
-    }
+  theEditor.commands.addCommand({
+    name: 'helpR',
+    bindKey: {win: 'F1', mac: 'F1'},
+    exec: function(editor) {
+      console.log('--------------helpR!----------------------');
+      editor.getSession().selection.moveCursorLongWordLeft();
+      editor.getSession().selection.selectWordRight();
+      var text = editor.getSession().getTextRange(editor.getSelectionRange());
+      Shiny.onInputChange('helpMssg', {query:text, num:Math.random(), editorId: id} );
+    } //TODO: remove editorVar; appears not to be used
   });
+      
+  theEditor.on("guttermousedown", function(e) {
+   // derived from the following link
+   //https://stackoverflow.com/questions/16864236/how-to-mark-line-numbers-in-javascript-ace-editor
+    var target = e.domEvent.target; 
+    if (target.className.indexOf("ace_gutter-cell") == -1)
+        return; 
+    if (!theEditor.isFocused()) 
+        return; 
+    if (e.clientX > 25 + target.getBoundingClientRect().left) 
+        return; 
+    var breakpoints = e.editor.session.getBreakpoints(row, 0);
+    var row = e.getDocumentPosition().row;
+    if(typeof breakpoints[row] === typeof undefined)
+        e.editor.session.setBreakpoint(row);
+    else
+        e.editor.session.clearBreakpoint(row);
+    e.stop();
+ });
+     
+ theEditor.on("input", function() {
+   //console.log('oninput');
+   //console.log("isSaved()==" + JSON.stringify(theEditor.session.getUndoManager().isSaved()));
+   //console.log("wasSaved()==" + JSON.stringify(theEditor.session.getUndoManager().$wasSaved ));
+   setTimeout(function(){
+     if(theEditor.session.getUndoManager().isSaved() != theEditor.session.getUndoManager().$wasState){
+     theEditor.session.getUndoManager().$wasSaved=theEditor.session.getUndoManager().isSaved();
+     //send message with updated saved status
+     //console.log('about to send message');
+     
+     Shiny.onInputChange('messageFromAce', 
+  		{
+  		     // code :  theEditor.getSession().getValue(),
+  		     //dirty: editor.getSession().getUndoManager().dirtyCounter,
+  		     isSaved: theEditor.session.getUndoManager().isSaved(),
+  		     sender : 'saveStatusUpdate',
+  		     id:id
+  		});
+  		
+   } 
+   }, 50
+   );
+   
+   
+ });
+ 
+ //  theEditor.session.getUndoManager().reset();
+
+    
+  $el.droppable({
+
+  activeClass: "ui-state-default",
+  hoverClass: "ui-state-hover",
+  accept: ":not(.ui-sortable-helper)",
+
+  drop: function(event, ui) {
+    var pos = $el.data('aceEditor').renderer.screenToTextCoordinates(event.clientX, event.clientY);
+    
+    console.log("pos=" + JSON.stringify(pos));
+    console.log("ui =\n" + JSON.stringify(ui));
+    var txt =  ui.draggable.attr("data-snippet");
+    console.log(
+      'txt=' + JSON.stringify(txt)
+    );
+    $el.data('aceEditor').moveCursorToPosition(pos);
+    $el.data('aceEditor').focus();
+    var tab_press= jQuery.Event('keydown', {which: 88});
+     
+    console.log('drop occurred');
+     $el.trigger(tab_press);
+     var snippetManager = ace.require("ace/snippets").snippetManager;
+    snippetManager.insertSnippet($el.data('aceEditor'), txt);
+
+    return true;
+  }
+});
   
   var geRandomString = function(length) {
                 var text = '';
@@ -256,18 +285,7 @@ function ptRaceInit(data){
               return text;
             };
             
-/*  Shiny.onInputChange('messageFromAce', 
-          {
-             code :  theEditor.getSession().getValue(),
-             sender : 'cmd.file.new',
-             id : data.id,
-             //dirty: editor.getSession().getUndoManager().dirtyCounter,
-             mode: data.mode,
-             isSaved: ud.isSaved(),
-             rnd : geRandomString(5)
-          }
-    );
-*/
+
 
 }
 
