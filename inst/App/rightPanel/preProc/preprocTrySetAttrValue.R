4b720051580629815bd41c0f2848f5d301b5546c
m. s. legrand <slegrand3.14@gmail.com>
2019-07-11 16:01:56 -0400

    cleanup preprocTrySetAttrValue

--- inst/App/rightPanel/preProc/preprocTrySetAttrValue.R
+++ inst/App/rightPanel/preProc/preprocTrySetAttrValue.R
@@ -5,53 +5,39 @@ preprocTrySetAttrValue<-function( cmd.Row, ptDefs, rowIndex, selection){
 
   tryCatch({ 
     txt<-getPreProcPtScript()['onChangeRow']
-    newPtDefs<-ptDefs
+    #newPtDefs<-ptDefs
     tibs<-ptDefs$tib
     tib<-tibs[[selection]]
     values<-tib[[getTibColumnName()]]
-    cat('here A\n')
     if(identical(class(values),'list')){
       stop('cannot edit list column')
     }
-    cat('here B\n')
-    print(values)
     getAttrValue<-function(){values[rowIndex]}
-    cat(getAttrValue(),"\n")
     context<-list(
       name=getAssetName(),
       column=getTibColPos(),
       row=rowIndex,
       tibs=tibs
     )
-    cat('here C\n')
-    print(context)
     ppenv<-list(
       setAttrValue=setAttrValue,
       getAttrValue=getAttrValue,
       context=context,
       keys=list(alt=mssg$altKey, shift=mssg$shiftKey, ctrl=mssg$ctrlKey, meta=mssg$metaKey)
     )
-    cat('here D\n')
     tibs<-eval(parse(text=txt), ppenv )
-    cat('here F\n')
-    print(tibs$points)
-    
     validateTibLists(getPtDefs()$tib, tibs)
-    cat('here G\n')
-    newPtDefs$tib<-tibs
-    cat('here H\n')
-    if(!is.null(tibs)){ #update only upon success
-      cat('here I\n')
-      #sender='cmd.commit'
-      # sender='cmd.valueChange'
-      # updateAceExtDef(newPtDefs, sender=sender, selector=list( rowIndex=rowIndex ))
-      sender='applyTibEdit'
-      updateAceExtDef(newPtDefs, sender=sender, selector=list( name=context$name, rowIndex=context$row   ) )
-    }
+    #newPtDefs$tib<-tibs
+    ptDefs$tib<-tibs
+    # if(!is.null(tibs)){ #update only upon success
+    #   sender='applyTibEdit'
+    #   updateAceExtDef(newPtDefs, sender=sender, selector=list( name=context$name, rowIndex=context$row   ) )
+    # }
+    sender='applyTibEdit'
+    updateAceExtDef(ptDefs, sender=sender, selector=list( name=context$name, rowIndex=context$row   ) )
   }, error=function(e){
     e<-c('preproErr',e)
     err<-paste(unlist(e), collapse="\n", sep="\n")
-    # cat(err)
     alert(err)
   })
 }
@@ -69,7 +55,6 @@ preprocTrySetAttrValueS<-function(scripts,  ptDefs, rowIndex, selection){
       cols<-names(scripts)
       for(columnName in cols){
         txt<-scripts[columnName]
-        # browser()
         values<-tib[[columnName]]
         getAttrValue<-function(){values[rowIndex]}
         context<-list(
@@ -96,7 +81,6 @@ preprocTrySetAttrValueS<-function(scripts,  ptDefs, rowIndex, selection){
   }, error=function(e){
     e<-c('preproErr',e)
     err<-paste(unlist(e), collapse="\n", sep="\n")
-    # cat(err)
     alert(err)
   })
 }
