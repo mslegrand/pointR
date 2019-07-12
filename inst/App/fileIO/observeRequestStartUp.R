23a4efbdd696e4ec6c4acaba9d1027cc18998279
m. s. legrand <slegrand3.14@gmail.com>
2019-07-12 17:59:09 -0400

    commented out openFileNow, cmdFileNew, as they appear not to be used

--- inst/App/fileIO/observeRequestStartUp.R
+++ inst/App/fileIO/observeRequestStartUp.R
@@ -8,16 +8,14 @@ observeEvent(request$sender,{
     if(is.null(aceId)){
         cmdFileNewPtR()
     } else {
-      # updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE ) #??
+      
     }
     disableDMDM(
       session, 
       menuBarId="editNavBar", 
       entry="customControl"
     )
-    # updateAceExt(id=aceId, sender='cmd.file.new', getValue= TRUE,  ok=TRUE )
-    # resetShinyFilesIOPaths(getDirPath()) #lets just force this to happen
-    # dirtyDMDM(session, "editNavBar")
+    
     log.fout(startup)
   }
 }, priority=100)
