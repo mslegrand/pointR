4a6fc5b74c1ca5f1971428a2d3325972820da27f
m. s. legrand <slegrand3.14@gmail.com>
2019-10-06 15:40:06 -0400

    relaxed validation to allow for usage of NA for non-list numeric of character cols

--- inst/App/rightPanel/preProc/preProcValidate.R
+++ inst/App/rightPanel/preProc/preProcValidate.R
@@ -1,13 +1,24 @@
 
 assertConsistantTibPair<-function(tib1, tib2){
-  # assert names are equal
-  if(!setequal(names(tib1), names(tib2))){
-    stop('Inconsistency: returned missing or extra tibble columns')
-  }
+  # assert col names are equal
+  # if(!setequal(names(tib1), names(tib2))){
+  #   stop('Inconsistency: returned missing or extra tibble columns')
+  # }
   # assert coltypes are equal
-  for(n in names(tib1)){
-    if(!identical(extractColType(tib1[[n]]), extractColType(tib2[[n]]))){
-      stop('Operation not allowed: column type for column n cannot be changed')
+
+  for(nm in intersect(names(tib1), names(tib1)) ){
+    c1<-tib1[[nm]]
+    if(all(is.na(c1))){
+      next
+    }
+    c2<-tib2[[nm]]
+    if(all(is.na(c2))){
+      next
+    }
+    c1<-na.omit(c1)
+    c2<-na.omit(c2)
+    if(!identical(extractColType(c1), extractColType(c2))){
+      stop('Not allowed: this operation forbids the column type to be changed')
     }
   }
 }
@@ -15,10 +26,10 @@ assertConsistantTibPair<-function(tib1, tib2){
 validateTibLists<-function(tibs1, tibs2){
   Assert$check({!is.null(tibs2)}, message='returned null instead of a list')
   Assert$check({is.list(tibs2)}, message='fails to return a list of tibbles')
-  if(!setequal(names(tibs1), names(tibs2))){
-    stop('returned missing or extra ptR members')
-  }
-  for(n in names(tibs1)){
+  # if(!setequal(names(tibs1), names(tibs2))){
+  #   stop('returned missing or extra ptR members')
+  # }
+  for(n in intersect(names(tibs1),names(tibs2))){
     assertConsistantTibPair(tibs1[[n]], tibs2[[n]])
   }  
 }
