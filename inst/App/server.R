87fd1e457b761597f71b494da31fe72dc8f202df
mslegrand <slegrand3.14@gmail.com>
2019-10-15 22:41:33 -0400

    source serverAuxPreproc.R

--- inst/App/server.R
+++ inst/App/server.R
@@ -25,6 +25,7 @@ shinyServer(function(input, output,session) {
   source("util/utilTransform.R")
   source("util/copyAndRenameProject.R")
   source("fileIO/dndSnippetLoader.R")
+  source("rightPanel/preProc/serverAuxPreproc.R", local=TRUE)
   source("rightPanel/preProc/preProcSetAttr.R")
   source("rightPanel/preProc/preprocTrySetAttrValue.R", local=TRUE)
   source("rightPanel/preProc/preProcValidate.R")
