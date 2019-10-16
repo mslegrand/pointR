555c67fdc0db19e858e453fd8bd6fcdefaa7064b
mslegrand <slegrand3.14@gmail.com>
2019-10-15 22:46:53 -0400

    newPreProcPoint

--- inst/App/rightPanel/preProc/pointPreprocessor.R
+++ inst/App/rightPanel/preProc/pointPreprocessor.R
@@ -47,3 +47,41 @@ newPointPreprocessor<-function()
                  )
   ) 
 }
+
+
+# dropdown for preprocessor
+newPreProcDropDown<-function()
+{ 
+  # ppc<-getPreProcChoices()
+  # ppcNames<-sub('\\.R$', '', ppc)
+  dropdownId= "ptPreProcDropDown" #paste0("PtPreProc",id)
+  absolutePanel( id='PtPreProcDiv', left=5, bottom=5, 
+                 dropdown( inputId = dropdownId ,  
+                           div( id='ptPreProcBackPanel', class='backPanel',
+                                div( style='margin-left:20px; color: #00ffff; ', h4(textOutput("ptPreProcSource")) ) ,
+                                radioButtons(inputId = 'preProcChooser', 
+                                             label = 'Preproc chooser', 
+                                             choiceNames=c('none'),
+                                             choiceValues= c('none'),
+                                             width="100%"
+                                ),
+                                
+                                div(style="width: 100%; overflow: hidden; margin-top:5px; margin-bottom:5px",
+                                    div( style="float:left;",
+                                         actionButton(inputId= "commitPtPreProcButton", label='Commit', class=c("btn") )
+                                    ),
+                                    div( style="float:right;",
+                                         actionButton(inputId= "dimissPtPreProcButton", label='Dismiss', class=c("btn") )
+                                    )
+                                )
+                           ),
+                           icon=icon("filter", lib = "glyphicon"), #icon=icon("wrench", lib = "glyphicon"), #icon = icon("toolbox"),
+                           status = "primary", width = "400px", size='sm',
+                           up=TRUE, right=FALSE,
+                           animate = animateOptions(
+                             enter = animations$fading_entrances$fadeInLeftBig,
+                             exit  = animations$fading_exits$fadeOutRightBig
+                           )
+                 )
+  ) 
+}
