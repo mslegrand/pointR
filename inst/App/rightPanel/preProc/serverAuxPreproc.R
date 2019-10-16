c01504b06772f554a43101d92817de3d86a24358
mslegrand <slegrand3.14@gmail.com>
2019-10-15 22:46:25 -0400

    moved preProcScriptDB added readAuxPreProcs fix loadAuxPreProcs

--- inst/App/rightPanel/preProc/serverAuxPreproc.R
+++ inst/App/rightPanel/preProc/serverAuxPreproc.R
@@ -1,5 +1,9 @@
+preProcScriptDB<-reactiveValues(
+  points=tibble( scriptName='bogus', cmd="bogus", script='bogus')[0,],
+  attrs=tibble( scriptName='bogus', cmd="bogus", script='bogus')[0,]
+)
 
-loadAuxPreProc<-function(datapath, type){
+loadAuxPreProc<-function(fullName){
   extractBodyWithComments<-function(fn){
     tt<-capture.output(print(fn))
     tt<-paste(tt, collapse="\n")
@@ -12,7 +16,7 @@ loadAuxPreProc<-function(datapath, type){
     substr(tt,pos1,pos2)
   }
   tryCatch({
-    preProcList<-source(datapath, local=T)$value
+    preProcList<-source(fullName, local=T)$value
     #check preProcList
     if(is.null(preProcList) ||  
        #length(preProcList)!=3 ||
@@ -22,12 +26,13 @@ loadAuxPreProc<-function(datapath, type){
       stop('ill-formed  preprocessor')
       # todo better message
     }
-    tb<-tibble(scriptName=basename(datapath), cmd=names(preProcList), script=preProcList)
+    tb<-tibble(scriptName=basename(fullName), cmd=names(preProcList), script=preProcList)
     # may need to use cmds instead
-    if( "preprocPts"==basename(dirname(filePath))){
-      rbind(preProcScriptDB$points, tb)
-    } else if( "preprocAts"==basename(dirname(filePath))){
-      rbind(preProcScriptDB$attrs, tb)
+    # browser()
+    if( "preprocPts"==basename(dirname(fullName))){
+      preProcScriptDB$points<-rbind(preProcScriptDB$points, tb)
+    } else if( "preprocAts"==basename(dirname(fullName))){
+      preProcScriptDB$attrs<-rbind(preProcScriptDB$attrs, tb)
     }
   }, 
   error=function(e){
@@ -40,8 +45,41 @@ loadAuxPreProc<-function(datapath, type){
 reloadPreProcScriptDB<-function(dirPath, scriptType='points'){
   ppfiles<-dir(dirPath, full.names=TRUE)
   # refresh/add any dnd whose file has just appeared
-  for(datapath in ppfiles ){
+  for(fullName in ppfiles ){
     # loadPreProc
-    loadAuxPreProc(datapath)
+    readAuxPreProc(fullName)
   }
 }
+
+
+getPreProcPPAuxPath<-reactive({file.path(getDirPath(),'aux','preprocPts')})   
+getPreProcPAAuxPath<-reactive({file.path(getDirPath(),'aux','preprocAts')})   
+
+readAuxPreProcs<-function( startup=TRUE){
+  
+  preProcFilePaths<-c(
+    list.files(getPreProcPPAuxPath(), full.names=TRUE),
+    list.files(getPreProcPAAuxPath(), full.names=TRUE)
+  ) 
+  for(fp in preProcFilePaths){
+    loadAuxPreProc(fp)
+  }
+}
+
+
+
+getPreProcChoices<-reactive({
+  rtv<-c()
+  if( getTibEditState()==TRUE ){
+    browser()
+    
+    if(getColumnType()=='point'){
+      rtv<-unique(preProcScriptDB$points$scriptName)
+    } else {
+      rtv<-unique(preProcScriptDB$attrs$scriptName)
+    }
+  }
+  rtv
+})
+
+hasPreProcChoices<-reactive({ length(getPreProcChoices)>0})
