422251d6c0020800cfc2bf7b4bdf107cc7067877
m. s. legrand <slegrand3.14@gmail.com>
2019-12-06 11:32:27 -0500

    reformat svgR templated

--- inst/App/templates/svgRTemplate.R
+++ inst/App/templates/svgRTemplate.R
@@ -4,7 +4,11 @@ WH<-c(600,400)
 
 svgR(wh=WH,
      #your custom code goes here
-    lapply(1:150, 
-            function(i){ line(xy1=WH/2, xy2=sample(200:300,2), stroke='red')
-    })
+     lapply(1:150, function(i){ 
+       line(
+         xy1=WH/2, 
+         xy2=sample(200:300,2), 
+         stroke='red'
+       )
+     })
 )
