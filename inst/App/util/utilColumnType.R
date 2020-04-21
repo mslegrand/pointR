8c0660cff322898dd1afb4374a9e24875791369b
stephen legrand <sup@stephens-Mac-mini.local>
2020-04-21 16:26:09 -0400

    edited isPoints to be more robust

--- inst/App/util/utilColumnType.R
+++ inst/App/util/utilColumnType.R
@@ -15,10 +15,10 @@ isColorString <- function(x) {
   }))
 }
 
-# sole caller: listColType (below)
+# takes a  column x and checks if the entries are all 2x* matrices of numerics
 isPoints<-function(x){
-  points<-!is.null(x) && all(unlist(lapply(x, function(m){
-    is.matrix(m) && dim(m)[1]==2
+  points<-is.list(x) &&  all(unlist(lapply(x, function(m){
+    is.matrix(m) && dim(m)[1]==2 # && all(apply(m,1,is.numeric))
   })))
   points
 }
@@ -37,6 +37,7 @@ charColType<-function(x){
 
 #sole caller: extractColType (below)
 listColType<-function(x){
+  
   lens<- sapply(x, length)
   if(isPoints(x)){
     return("point")
@@ -70,6 +71,7 @@ listColType<-function(x){
 
 # callers: PanelCoordinator::getColumnType, preProcValidate::assertConsistantTibPair
 extractColType<-function( column ){
+  
   if(is.list(column)){
     return(listColType(column))
   } else {
