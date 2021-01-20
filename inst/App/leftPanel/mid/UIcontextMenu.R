ad5e5e47da7330c8b4c245b51636f4ab9daf38cc
m. s.  legrand <slegrand3.14@gmail.com>
2021-01-20 08:42:31 -0500

    adding Edit Code Block to context menu

--- inst/App/leftPanel/mid/UIcontextMenu.R
+++ inst/App/leftPanel/mid/UIcontextMenu.R
@@ -9,7 +9,8 @@ UIcontextMenu<-function(){
              tag('li',list(class='clickMe', span(class='icon-clone'),span('Copy')  )),
              tag('li',list(class='clickMe', span(class='icon-scissors'),span('Cut')  )),
              tag('li',list(class='clickMe', span(class='icon-paste'),span('Paste')  )),
-             tag('li',list(class='clickMe', span(class='icon-cancel'),span('Delete')  ))
+             tag('li',list(class='clickMe', span(class='icon-cancel'),span('Delete')  )),
+             tag('li',list(id='rmd-edit-code', class='clickMe', span(class='icon-file-code'),span('Edit Code Block')  ))
            )),
            hr(class='contexthr')
          )
@@ -18,7 +19,8 @@ UIcontextMenu<-function(){
        },
        tag('ul', list(
          id='items',
-         tag('li',list(class='clickMe', span(class="icon-help"), span( 'Lookup element')))
+         tag('li',list(class='clickMe', span(class="icon-help"), span( 'Lookup element'))),
+         tag('li',list(id='rmd-edit-code', class='clickMe', span(class='icon-file-code'),span('Edit Code Block')  ))
        ))
   )
 }
