26cfc6cfbd192842f8eddb5a25f759a132d8cfc9
m. s.  legrand <slegrand3.14@gmail.com>
2021-01-20 08:43:31 -0500

    add cmd to update dependents if Rmd

--- inst/App/leftPanel/footer/processKnit.R
+++ inst/App/leftPanel/footer/processKnit.R
@@ -3,6 +3,8 @@ processKnit<-reactive({
   # cat_list<<-c( cat_list,">---> processKnit\n")
   clearErrorMssg()
   #src<-request$code
+  updateAceExt( id= getAceEditorId(), sender='commit.removeMarkers', removeAllMarkers='removeAllMarkers', updateRmdDependents=getAceEditorId() )
+  
   src<-getCode()
   
   setSourceType(rmdPanelTag)
