dae0d65e449a6a69b308db6c49d53af9988362ef
m. s.  legrand <slegrand3.14@gmail.com>
2021-02-07 19:54:38 -0500

    save only if fileDescriptor exists

--- inst/App/fileIO/serverPage2Workspace.R
+++ inst/App/fileIO/serverPage2Workspace.R
@@ -8,30 +8,32 @@ savePage<-function(pageId, path=getWorkSpaceDir()){
     fileName=paste0(path,"/",pageId,".rda")
     asel<-reactiveValuesToList(selectedAsset)
     fileDescriptor=getFileDescriptor(pageId)
-    backdrop=getPageBackDrop(pageId)
-    grid=getPageSvgGrid(pageId)
-    trib<-getPageUseTribble(pageId)
-    dnip<-getPageDnippetsDB(pageId)
-    widg<-getPageWidgetDB(pageId)
-    preprocPage<-getPagePreprocPageDB(pageId)
-    rtv<-c(
-      fileDescriptor=getFileDescriptor(pageId),
-      code=getCode(),
-      assetSelection=asel,
-      backdrop=backdrop,
-      grid=grid,
-      trib=trib,
-      dnip=dnip,
-      widg=widg,
-      preprocPage=preprocPage  
-    )
-    
-    # ppE<-getPreProcPtEntries(pageId)
-    # if(length(ppE)!=0 && nrow(ppE)>0){
-    #   rtv<-c(rtv, preprocScripts=ppE)
-    # }
-    if(length(preprocPage))
-    saveRDS(object=rtv, file = fileName)
+    if(length(fileDescriptor)>0){
+      backdrop=getPageBackDrop(pageId)
+      grid=getPageSvgGrid(pageId)
+      trib<-getPageUseTribble(pageId)
+      dnip<-getPageDnippetsDB(pageId)
+      widg<-getPageWidgetDB(pageId)
+      preprocPage<-getPagePreprocPageDB(pageId)
+      rtv<-c(
+        fileDescriptor=getFileDescriptor(pageId),
+        code=getCode(),
+        assetSelection=asel,
+        backdrop=backdrop,
+        grid=grid,
+        trib=trib,
+        dnip=dnip,
+        widg=widg,
+        preprocPage=preprocPage  
+      )
+      
+      # ppE<-getPreProcPtEntries(pageId)
+      # if(length(ppE)!=0 && nrow(ppE)>0){
+      #   rtv<-c(rtv, preprocScripts=ppE)
+      # }
+      # if(length(preprocPage))
+      saveRDS(object=rtv, file = fileName)
+    }
     log.fout(savePage)
   }
 }
